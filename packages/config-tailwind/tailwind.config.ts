7cb0c6ddb513911d4a5442a8d22ecc08cfb45e57
Yongjae Choi <yongjae.choi20@gmail.com>
2024-12-26 22:31:18 +0900

    [#4/ui] feat: 디자인 토큰(색상) 및 유틸리티 설정 (#8)
    * feat: tailwind에 프로젝트 토큰 설정
    
    * chore: docs/storybook의 ui 패키지 종속 수정
    
    * feat: tailwind cn 유틸함수 구현
    
    * chore: config-tailwind exports에 variables 경로 추가
    
    * chore: ui 패키지에 tailwind-config/variables 연결
    
    * chore: ui 패키지 tailwind prefix 제거
    
    * style: primary, secondary 색상 토큰에 shade속성 추가
    
    * chore: docs/storybook에 tailwind-config/variables 연결
    
    * fix: tailwind-config 패키지 경로 오타 수정
    
    * style: 프로젝트 색상 css 변수 hsl 단위로 변경
    
    * docs: Colors 명세서 작성
    
    * refactor: cn 유틸함수를 컨벤션에 맞게 화살표함수로 변경
    
    * chore: utils.ts경로 exports 정의
    
    * chore: ui/src/components 경로 생성

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -2,7 +2,52 @@ import type { Config } from "tailwindcss";
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          primary: {
+            100: 'hsl(241 100 93)',
+            200: 'hsl(241 100 87)',
+            300: 'hsl(241 100 80)',
+            400: 'hsl(241 100 73)',
+            500: 'hsl(241 100 67)',
+            600: 'hsl(241 57 53)',
+            700: 'hsl(241 50 40)',
+            800: 'hsl(241 50 27)',
+            900: 'hsl(241 50 13)',
+          },
+          secondary: {
+            100: 'hsl(14 100 94)',
+            200: 'hsl(14 100 88)',
+            300: 'hsl(14 100 83)',
+            400: 'hsl(14 100 77)',
+            500: 'hsl(14 100 71)',
+            600: 'hsl(14 54 57)',
+            700: 'hsl(14 41 43)',
+            800: 'hsl(14 41 28)',
+            900: 'hsl(14 41 14)',
+          },
+        },
+        text: {
+          primary: "hsl(var(--text-primary))",
+          sub1: "hsl(var(--text-sub1))",
+          sub2: "hsl(var(--text-sub2))",
+          sub3: "hsl(var(--text-sub3))",
+          sub4: "hsl(var(--text-sub4))",
+          sub5: "hsl(var(--text-sub5))",
+        },
+        background: {
+          primary: "hsl(var(--background-primary))",
+          sub1: "hsl(var(--background-sub1))",
+          sub2: "hsl(var(--background-sub2))",
+          sub3: "hsl(var(--background-sub3))",
+          sub4: "hsl(var(--background-sub4))"
+        },
+        notification: "hsl(var(--notification))",
+      }
+    }
+  },
   plugins: [],
 };
 export default config;
