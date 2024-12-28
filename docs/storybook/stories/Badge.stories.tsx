be3d91ff87540fae61f8ef1faab98154b304af65
yjc2021 <yongjae.choi20@gmail.com>
2024-12-28 16:21:06 +0900

    docs: Badge 스토리 작성

---
+++ docs/storybook/stories/Badge.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Badge } from "@5unwan/ui/badge";
+
+const meta: Meta<typeof Badge> = {
+  component: Badge,
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['default', 'sub1', 'sub2', 'brand', 'destructive']
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'medium', 'large']
+    }
+  },
+  args: {
+    children: "Badge",
+    size: 'medium'
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Badge>;
+
+export const Default: Story = {
+  args: {
+    variant: "default",
+    children: '00/20'
+  },
+};
+export const Sub1: Story = {
+  args: {
+    variant: 'sub1',
+    children: '00/20'
+  }
+}
+export const Sub2: Story = {
+  args: {
+    variant: 'sub2',
+    children: 'PT완료'
+  }
+}
+export const Brand: Story = {
+  args: {
+    variant: 'brand',
+    children: '00/20'
+  }
+}
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive'
+  }
+}
