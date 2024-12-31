1c5a2d8e16012bef906cea8bab212ab944996db4
choi-ik <ci980704@naver.com>
2024-12-31 11:12:03 +0900

    refactor: text 관련 tailwind utility 충돌을 extendTailwindMerge를 핸들링하여 스타일 충돌 해결

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,6 +1,25 @@
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+import resolveConfig from "tailwindcss/resolveConfig";
+
+import tailwindConfig from "../../tailwind.config.ts";
+
+const customTailwindTheme = resolveConfig(tailwindConfig).theme.fontSize;
+const customFontSizes = Object.keys(customTailwindTheme);
+
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    theme: {},
+    classGroups: {
+      "font-size": [
+        {
+          text: customFontSizes,
+        },
+      ],
+    },
+  },
+});
 
 export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 };
