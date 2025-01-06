5529ecdf6b35ebe614d89e0613c7235d93e92a55
choi-ik <ci980704@naver.com>
2025-01-06 16:45:49 +0900

    PRIZ-34 test: Stepper 공통 컴포넌트의 props 네이밍이 변경됨에 따라 테스트 코드의 렌더 컴포넌트 props 변경

--- packages/ui/src/components/__tests__/Stepper.spec.tsx
+++ packages/ui/src/components/__tests__/Stepper.spec.tsx
@@ -1,6 +1,6 @@
 import userEvent from "@testing-library/user-event";
 
-import { render, screen } from "test-utils"; // 커스텀 render 사용
+import { render, screen } from "test-utils";
 
 import Stepper from "../Stepper";
 
@@ -8,7 +8,7 @@ describe("Stepper Component", () => {
   const mockGetChangeValue = jest.fn();
 
   it("+ 버튼 클릭을 통해  onChange 함수가 실행된다.", async () => {
-    render(<Stepper getChangeValue={mockGetChangeValue} />);
+    render(<Stepper onChangeValue={mockGetChangeValue} />);
 
     const increaseButton = screen.getByLabelText("increase");
 
@@ -18,7 +18,7 @@ describe("Stepper Component", () => {
   });
 
   it("- 버튼 클릭을 통해  onChange 함수가 실행된다.", async () => {
-    render(<Stepper getChangeValue={mockGetChangeValue} />);
+    render(<Stepper onChangeValue={mockGetChangeValue} />);
 
     const decreaseButton = screen.getByLabelText("decrease");
 
