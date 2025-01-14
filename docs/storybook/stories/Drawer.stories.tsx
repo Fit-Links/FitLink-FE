f74f2035392b210dcebfccd0efa8258d9e645760
MaSeungHyun <tpdlqj0514@gmail.com>
2025-01-15 01:40:48 +0900

    FLIZ-19 docs: Drawer 스토리 작성

---
+++ docs/storybook/stories/Drawer.stories.tsx
@@ -0,0 +1,43 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@5unwan/ui/components/Drawer";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta = {
+  component: Drawer,
+};
+export default meta;
+
+type Story = StoryObj<typeof Drawer>;
+
+export const Default: Story = {
+  args: {
+    shouldScaleBackground: true,
+  },
+  render: () => (
+    <div>
+      <Drawer>
+        <DrawerTrigger asChild={true}>
+          <button>Open</button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>
+              <div className="text-title-1 flex items-center justify-between">
+                알림
+                <DrawerClose className="text-[20px]">X</DrawerClose>
+              </div>
+            </DrawerTitle>
+            <DrawerDescription></DrawerDescription>
+          </DrawerHeader>
+        </DrawerContent>
+      </Drawer>
+    </div>
+  ),
+};
