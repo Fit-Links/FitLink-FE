59919c0a2ca8b32b0f8d029ce39b2d3bddb912c4
MaSeungHyun <tpdlqj0514@gmail.com>
2025-01-17 07:09:25 +0900

    FLIZ-16 feat: input caret blink 애니메이션 설정

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -114,6 +114,15 @@ const config: Omit<Config, "content"> = {
           },
         ],
       },
+      keyframes: {
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
+      },
     },
   },
   plugins: [
