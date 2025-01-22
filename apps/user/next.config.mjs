db6401a67f7b7f6780d73b372e9b6deaf68f6afc
Ik <ci980704@gmail.com>
2025-01-23 00:04:13 +0900

    [FILZ-31/root] chore: 각 패키지별 path Alias 설정 (#57)
    * FLIZ-31 chore: ui 패키지 path alias 설정
    
    * FLIZ-31 chore: storybook 패키지 path alias 설정
    
    * FLIZ-31 chore: user 도메인 path alias 설정
    
    * FLIZ-31 chore: trainer 도메인 path alias 설정
    
    * FLIZ-31 chore: trainer, user 도메인에서 ui 패키지의 path alias를 이해할 수 있도록 nextjs.json의 path alias 설정
    
    * FLIZ-31 chore: trainer, user 도메인에서 빌드 시 ui 패키지 alias를 parsing할 수 있도록 webpack 설정
    
    * FLIZ-31 chore: ui 패키지 alias 네이밍 변경
    
    * FLIZ-31 chore: trainer, user 도메인 패키지 alias 충돌로 인해 path alias 네이밍 수정
    
    * FLIZ-31 chore: 스토리북 ui 패키지 Path alias 네이밍 변경에 따라 함께 수정
    
    * FLIZ-31 chore: path alias 충돌로 인해 Nextjs.json에서 중앙 관리되던 baseUrl, paths 모두 제거

--- apps/user/next.config.mjs
+++ apps/user/next.config.mjs
@@ -1,7 +1,21 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@5unwan/ui"],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@ui": path.resolve(__dirname, "../../packages/ui/src"), // __dirname 사용
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
