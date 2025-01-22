1f435e06c3b8f0ca14ac2a8378e059a86bb4b9db
yjc2021 <yongjae.choi20@gmail.com>
2025-01-22 09:42:43 +0900

    FLIZ-32 rename: Context.tsx 소문자 네이밍으로 변경

--- packages/ui/src/hooks/useDayOfWeekPickerContext.ts
+++ packages/ui/src/hooks/useDayOfWeekPickerContext.ts
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-import DayOfWeekPickerContext from "../components/DayOfWeekPicker/Context";
+import DayOfWeekPickerContext from "../components/DayOfWeekPicker/context";
 
 export const useDayOfWeekPickerContext = () => {
   const context = useContext(DayOfWeekPickerContext);
