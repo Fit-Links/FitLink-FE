207d7ce1ca53e4a00bb6190bb85d841a34d8e991
yjc2021 <yongjae.choi20@gmail.com>
2025-01-23 10:28:52 +0900

    FLIZ-30 refactor: build.sh 불필요 스크립트 제거

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-cd ../
+# Ensure a clean slate
+rm -rf output
 mkdir output
+
+# Copy necessary content to the output directory
 cp -R ./FitLink-FE/* ./output
-cp -R ./output ./FitLink-FE
