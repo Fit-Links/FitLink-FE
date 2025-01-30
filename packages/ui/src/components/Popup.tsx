f09baa9e4193db5b828fbec5b97379d14f7f14d5
MaSeungHyun <tpdlqj0514@gmail.com>
2025-01-30 21:25:41 +0900

    FLIZ-85 FLIZ-26 feat: Popup Overlay추가 및 컴포넌트 구조 변경

--- packages/ui/src/components/Popup.tsx
+++ packages/ui/src/components/Popup.tsx
@@ -1,16 +1,63 @@
 "use client";
 
+import { Portal } from "@radix-ui/react-portal";
 import React, { forwardRef } from "react";
 
 import { cn } from "../lib/utils";
 
-const Popup = forwardRef<HTMLDivElement, React.ComponentPropsWithoutRef<"div">>(
+type PopupProps = {
+  open?: boolean;
+} & React.ComponentPropsWithoutRef<"div">;
+
+const Popup = forwardRef<HTMLDivElement, PopupProps>(
+  ({ children, className, open = false, ...props }, ref) => {
+    if (open)
+      return (
+        <div ref={ref} className={cn(className)} {...props}>
+          {children}
+        </div>
+      );
+  },
+);
+Popup.displayName = "Popup";
+
+const PopupPortal = forwardRef<
+  HTMLDivElement,
+  {
+    children: React.ReactNode;
+    container?: Element | DocumentFragment | null;
+  }
+>(({ children, container }, ref) => {
+  return (
+    <Portal ref={ref} container={container}>
+      {children}
+    </Portal>
+  );
+});
+PopupPortal.displayName = "PopupPortal";
+
+type PopupContentProps = {
+  container?: Element | DocumentFragment | null;
+} & React.ComponentPropsWithoutRef<"div">;
+
+const PopupContent = forwardRef<HTMLDivElement, PopupContentProps>(
+  ({ children, container }, ref) => (
+    <PopupPortal ref={ref} container={container}>
+      <PopupOverlay>
+        <PopupContainer>{children}</PopupContainer>
+      </PopupOverlay>
+    </PopupPortal>
+  ),
+);
+PopupContent.displayName = "PopupContent";
+
+const PopupOverlay = forwardRef<HTMLDivElement, React.ComponentPropsWithoutRef<"div">>(
   ({ children, className, ...props }, ref) => {
     return (
       <div
         ref={ref}
         className={cn(
-          "text-text-primary fixed inset-0 left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black/75",
+          "text-text-primary fixed inset-0 left-0 top-0 z-50 flex h-auto w-auto items-center justify-center bg-black/75",
           className,
         )}
         {...props}
@@ -20,7 +67,7 @@ const Popup = forwardRef<HTMLDivElement, React.ComponentPropsWithoutRef<"div">>(
     );
   },
 );
-Popup.displayName = "Popup";
+PopupOverlay.displayName = "PopupOverlay";
 
 const PopupContainer = forwardRef<HTMLDivElement, React.ComponentPropsWithoutRef<"div">>(
   ({ children, className, ...props }, ref) => {
@@ -38,8 +85,8 @@ const PopupContainer = forwardRef<HTMLDivElement, React.ComponentPropsWithoutRef
     );
   },
 );
-
 PopupContainer.displayName = "PopupContainer";
+
 const PopupTitle = forwardRef<HTMLDivElement, React.ComponentPropsWithoutRef<"div">>(
   ({ children, className, ...props }, ref) => {
     return (
@@ -85,4 +132,4 @@ const PopupButtonGroup = forwardRef<HTMLDivElement, React.ComponentPropsWithoutR
 );
 PopupButtonGroup.displayName = "PopupButtonGroup";
 
-export { Popup, PopupContainer, PopupTitle, PopupDescription, PopupButtonGroup };
+export { Popup, PopupContent, PopupTitle, PopupDescription, PopupButtonGroup };
