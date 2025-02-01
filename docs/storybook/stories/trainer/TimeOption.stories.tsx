ddfc17bb3d36818d06a22cb2f874e36bf2719d47
choi-ik <ci980704@naver.com>
2025-02-01 18:42:52 +0900

    FLIZ-53 docs: 변경된 TimeOption 컴포넌트의 내부 동작에 알맞게 스토리 수정

--- docs/storybook/stories/trainer/TimeOption.stories.tsx
+++ docs/storybook/stories/trainer/TimeOption.stories.tsx
@@ -1,65 +1,68 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { CalendarClock, CalendarMinus2, CalendarX2, Dumbbell } from "lucide-react";
 
-import TimeOption from "trainer/components/TimeOption";
+import { TimeOption, TimeOptionContent, TimeOptionIcon } from "trainer/components/TimeOption";
 
 const meta: Meta<typeof TimeOption> = {
   component: TimeOption,
   tags: ["autodocs"],
-  args: {
-    optionIcon: <Dumbbell className="text-text-primary" />,
-    children: <div>PT 예약</div>,
-  },
-  argTypes: {
-    optionIcon: {
-      control: "object",
-      description: "TimeOption의 icon으로 사용할 icon components prop",
-    },
-    children: {
-      control: "object",
-      description: "TimeOption의 상위에서 주입 받아 텍스트로 활용될 children prop",
-    },
-  },
 };
 
 export default meta;
 
 type TimeOptionStory = StoryObj<typeof TimeOption>;
 
-export const PtReservation: TimeOptionStory = {};
+export const PtReservation: TimeOptionStory = {
+  render: () => (
+    <TimeOption>
+      <TimeOptionIcon>
+        <Dumbbell className="text-text-primary" />
+      </TimeOptionIcon>
+      <TimeOptionContent>
+        <div>PT 예약</div>
+      </TimeOptionContent>
+    </TimeOption>
+  ),
+};
 
 export const PtFixedReservation: TimeOptionStory = {
-  args: {
-    optionIcon: <CalendarClock className="text-text-primary" />,
-  },
-  render: (args) => (
-    <TimeOption {...args}>
-      <div>PT</div>
-      <div>고정 예약</div>
+  render: () => (
+    <TimeOption>
+      <TimeOptionIcon>
+        <CalendarClock className="text-text-primary" />
+      </TimeOptionIcon>
+      <TimeOptionContent>
+        <div>PT</div>
+        <div>고정 예약</div>
+      </TimeOptionContent>
     </TimeOption>
   ),
 };
 
 export const PtReservationNotPossible: TimeOptionStory = {
-  args: {
-    optionIcon: <CalendarX2 className="text-text-primary" />,
-  },
-  render: (args) => (
-    <TimeOption {...args}>
-      <div>예약 불가</div>
-      <div>시간대 등록</div>
+  render: () => (
+    <TimeOption>
+      <TimeOptionIcon>
+        <CalendarX2 className="text-text-primary" />
+      </TimeOptionIcon>
+      <TimeOptionContent>
+        <div>예약 불가</div>
+        <div>시간대 등록</div>
+      </TimeOptionContent>
     </TimeOption>
   ),
 };
 
 export const HolidaySettings: TimeOptionStory = {
-  args: {
-    optionIcon: <CalendarMinus2 className="text-text-primary" />,
-  },
-  render: (args) => (
-    <TimeOption {...args}>
-      <div>휴무일</div>
-      <div>설정</div>
+  render: () => (
+    <TimeOption>
+      <TimeOptionIcon>
+        <CalendarMinus2 className="text-text-primary" />
+      </TimeOptionIcon>
+      <TimeOptionContent>
+        <div>휴무일</div>
+        <div>설정</div>
+      </TimeOptionContent>
     </TimeOption>
   ),
 };
