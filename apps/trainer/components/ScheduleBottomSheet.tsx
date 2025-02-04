f0f1cdc77bdc01b5484346f6b44ac1d635b9d6c5
choi-ik <ci980704@naver.com>
2025-02-04 15:45:26 +0900

    FLIZ-35 feat: TimeOptionList를 포함하는 ScheduleBottomSheet 컴포넌트 구현

--- apps/trainer/components/ScheduleBottomSheet.tsx
+++ apps/trainer/components/ScheduleBottomSheet.tsx
@@ -1,33 +1,33 @@
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "@ui/components/Sheet";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@ui/components/Sheet";
 import { cn } from "@ui/lib/utils";
 import { ReactNode } from "react";
 
 import { formatDateTimeWithDay } from "@trainer/utils/ScheduleBottomSheetUtils";
 
+import TimeOptionList from "./TimeOptionList";
+
 type ScheduleBottomSheetProps = {
   className?: string;
   selectedDate: Date;
-  children: ReactNode;
+  trigger: ReactNode;
 };
 
 export default function ScheduleBottomSheet({
   selectedDate,
   className,
-  children,
+  trigger,
 }: ScheduleBottomSheetProps) {
   return (
     <Sheet>
-      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetTrigger asChild>{trigger}</SheetTrigger>
       <SheetContent
         side={"bottom"}
-        className={cn("+ absolute left-1/2 h-[17rem] w-[24.563rem] -translate-x-1/2", className)}
+        className={cn("absolute left-1/2 h-[17rem] w-[24.563rem] -translate-x-1/2", className)}
       >
-        <SheetHeader className="text-text-primary text-title-1 items-center">
-          {formatDateTimeWithDay(selectedDate)}
+        <SheetHeader className="items-center">
+          <SheetTitle>{formatDateTimeWithDay(selectedDate)}</SheetTitle>
         </SheetHeader>
-        <div className="] mb-[1.625rem] ml-[1.063rem] mt-[1.25rem] flex items-center gap-1.5 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {/*TODO: TImeOption List가 들어올 예정 */}
-        </div>
+        <TimeOptionList />
       </SheetContent>
     </Sheet>
   );
