5cebb41eee8020de7a94894d1467fb56bdb86a83
choi-ik <ci980704@naver.com>
2025-02-04 16:31:54 +0900

    FLIZ-35 docs: ScheduleBottomSheet 컴포넌트 스토리 작성

---
+++ docs/storybook/stories/trainer/ScheduleBottomSheet.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ScheduleBottomSheet from "trainer/components/ScheduleBottomSheet";
+
+const meta: Meta<typeof ScheduleBottomSheet> = {
+  component: ScheduleBottomSheet,
+  tags: ["autodocs"],
+  args: {
+    selectedDate: new Date(),
+    trigger: <button className="rounded-full border p-3">임시 버튼 클릭</button>,
+  },
+  argTypes: {
+    selectedDate: {
+      control: "date",
+      description: "ScheduleBottomSheet의 헤더에 나타낼 날짜를 주입 받습니다.",
+    },
+    trigger: {
+      description: "ScheduleBottomSheet를 open 상태를 변경하는 트리거 컴포넌트를 주입 받습니다.",
+    },
+  },
+};
+
+export default meta;
+
+type ScheduleBottomSheetStory = StoryObj<typeof ScheduleBottomSheet>;
+
+export const Default: ScheduleBottomSheetStory = {
+  render: (args) => <ScheduleBottomSheet {...args} />,
+};
