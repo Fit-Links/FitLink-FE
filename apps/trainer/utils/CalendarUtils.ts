09436c7cdd73144d8e8c01e8c04f66cf819bd2d4
Ik <ci980704@gmail.com>
2025-02-04 10:57:53 +0900

    [FLIZ-34/trainer] feat: Trainer 도메인 예약/일정 관리 페이지의 Calendar 컴포넌트 구현 (#62)
    * FLIZ-34 chore: swiper, date-fns 라이브러리 install
    
    * FLIZ-34 chore: trainer 도메인에서 tailwind 관련 정보 사용할 숴 있도록 기본 세팅
    
    * FLIZ-34 feat: 각각의 시간 블록을 나타낼 TimeBlock 컴포넌트 구현
    
    * FLIZ-34 feat: 각각의 시간 블록(TimeBlock)을 요일 단위로 나타낼 DaySchedule 컴포넌트 구현
    
    * FLIZ-34 feat: 각 시간 블록을 담고 있는 DaySchedule을 주 단위로 나타낼 WeekSchedule 컴포넌트 구현
    
    * FLIZ-34 feat: 캘린더 상단에 월, 일, 요일을 나타내는 DayOfWeek 컴포넌트 구현
    
    * FLIZ-34 feat: 00시 부터 23시까지 시간 단위를 나타낼 TimeColumn 컴포넌트 구현
    
    * FLIZ-34 feat: 시간 블록과 날짜, 시간 단위를 포함하는 Calender 컴포넌트 구현
    
    * FLIZ-34 feat: 두 엘리먼트의 스크롤 높이를 동기화 시켜줄 수 있는 useSyncScroll 커스텀 훅 구현
    
    * FLIZ-34 feat: 현재 날짜가 맞는지 확인하는 isToday, 오늘 날짜가 포함된 한 주를 가져오는 getWeekDates util 함수 구현
    
    * FLIZ-34 feat: 일~토 순서로 요일 데이터 enum 타입 정의
    
    * FLIZ-34 refactor: Calendar의 슬라이드 숫자를 현재날짜를 기준으로 지난 1년, 이후 1년 총 105주로 고정
    
    * FLIZ-34 feat: 현재 날짜를 기준으로 offset 만큼 날짜를 이동시켜야 하는 getOffsetDate util 함수 구현
    
    * FLIZ-34 chore: docs/storybook에서 apps/trainer의 컴포넌트를 불러와 스토리로 작성할 수 있게 디펜던시 설정
    
    * FLIZ-34 chore: storybook에서 trainer 도메인의 코드에 tailwind 적용 및 trainer 도메인 path alias를 parsing 할 수 있도록 설정
    
    * FLIZ-34 docs: Calendar 컴포넌트 스토리 작성
    
    * FLIZ-34 refactor: curretMonth 상태를 currentWeek 상태에 의존하는 일반 변수로 변경
    
    * FLIZ-34 refactor: HTMLAttributes -> ComponentProps로 TimeBlock의 유틸리티 타입 변경 및 ReservationStateType 분리
    
    * FLIZ-34 chore: DaySchedule 컴포넌트 Todo 주석 작성

---
+++ apps/trainer/utils/CalendarUtils.ts
@@ -0,0 +1,39 @@
+const WEEK_LENGTH = 7;
+const STEP = 1;
+const INITIAL_OFFSET = 0;
+
+export const isToday = (date: Date) => {
+  const today = new Date();
+
+  return (
+    date.getFullYear() === today.getFullYear() &&
+    date.getMonth() === today.getMonth() &&
+    date.getDate() === today.getDate()
+  );
+};
+
+export const getWeekDates = (currentDate: Date) => {
+  const dates: Date[] = [];
+
+  const date = new Date(currentDate);
+
+  const dayOfWeek = date.getDay();
+
+  const lastSunday = new Date(date);
+  lastSunday.setDate(date.getDate() - dayOfWeek);
+
+  for (let i = 0; i < WEEK_LENGTH; i += STEP) {
+    const newDate = new Date(lastSunday);
+    newDate.setDate(lastSunday.getDate() + i);
+    dates.push(newDate);
+  }
+
+  return dates;
+};
+
+export const getOffsetDate = (date: Date, offsetDays = INITIAL_OFFSET) => {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + offsetDays);
+
+  return newDate;
+};
