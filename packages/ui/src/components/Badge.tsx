62ba941cc93c3922b33e4ab63f951ab57663af5e
yjc2021 <yongjae.choi20@gmail.com>
2025-02-06 15:26:29 +0900

    FLIZ-60 refactor: Badge에 DotWrapper 적용

--- packages/ui/src/components/Badge.tsx
+++ packages/ui/src/components/Badge.tsx
@@ -1,50 +1,20 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
+import DotWrapper from "./DotWrapper";
 import { cn } from "../lib/utils";
-import { parseNotification } from "../utils/dotWrapperUtils";
+import { colorVariants, notificationVariants } from "./variants/notificationVariants";
 
 const badgeVariants = cva(
   "inline-flex rounded-full font-medium transition-colors focus:outline-none border-transparent shadow",
   {
     variants: {
-      variant: {
-        default: "bg-background-sub4 text-text-sub5",
-        sub1: "bg-background-sub1 text-text-primary",
-        sub2: "bg-background-sub2 text-text-primary",
-        brand: "bg-brand-primary-500 text-text-primary",
-        destructive: "bg-notification text-text-primary",
-      },
+      variant: colorVariants,
       size: {
         xl: "h-[38px] text-[17px] min-w-[78px]",
         lg: "h-[32px] text-[17px] min-w-[78px]",
         md: "h-[29px] text-[15px] min-w-[63px]",
         sm: "h-[25px] text-[13px] min-w-[57px]",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "md",
-    },
-  },
-);
-
-const notificationVariants = cva(
-  "absolute flex items-center justify-center rounded-full text-[10px] focus:outline-none transition-colors shadow",
-  {
-    variants: {
-      variant: {
-        default: "bg-background-sub4 text-text-sub5",
-        sub1: "bg-background-sub1 text-text-primary",
-        sub2: "bg-background-sub2 text-text-primary",
-        brand: "bg-brand-primary-500 text-text-primary",
-        destructive: "bg-notification text-text-primary",
-      },
-      size: {
-        xl: "h-[18px] w-[18px] -right-[6px] -top-[6px]",
-        lg: "h-[18px] w-[18px] -right-[6px] -top-[6px]",
-        md: "h-[14px] w-[14px] -right-[5px] -top-[5px]",
-        sm: "h-[14px] w-[14px] -right-[5px] -top-[5px]",
       },
     },
     defaultVariants: {
@@ -64,7 +34,11 @@ export interface BadgeProps
 
 function Badge({ className, variant, size, notification, children, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ size }), "relative")}>
+    <DotWrapper
+      enabled={!!notification}
+      notification={notification}
+      notificationClassName={notificationVariants({ variant, size })}
+    >
       <div
         className={cn(
           badgeVariants({ variant, size }),
@@ -79,12 +53,7 @@ function Badge({ className, variant, size, notification, children, ...props }: B
       >
         {children}
       </div>
-      {notification && (
-        <span className={cn(notificationVariants({ variant, size }))}>
-          {parseNotification(notification)}
-        </span>
-      )}
-    </div>
+    </DotWrapper>
   );
 }
 
--- packages/ui/src/components/DotWrapper.tsx
+++ packages/ui/src/components/DotWrapper.tsx
@@ -2,20 +2,23 @@ import React from "react";
 
 import { cn } from "../lib/utils";
 import { parseNotification } from "../utils/dotWrapperUtils";
+import { notificationVariants } from "./variants/notificationVariants";
 
 type Props = {
   children: React.ReactNode;
   enabled: boolean;
-  notification: string;
+  notification?: string;
+  notificationClassName?: string;
 };
-function DotWrapper({ children, enabled, notification }: Props) {
+function DotWrapper({ children, enabled, notification = "", notificationClassName }: Props) {
   return (
-    <div className="relative">
+    <div className="relative w-fit">
       {children}
       {enabled && (
         <span
           className={cn(
-            "bg-brand-primary-500 text-text-primary absolute -right-[5px] -top-[5px] flex h-[14px] w-[14px] items-center justify-center rounded-full text-[10px] shadow transition-colors focus:outline-none",
+            notificationVariants({ variant: "brand", size: "xl" }),
+            notificationClassName,
           )}
         >
           {parseNotification(notification)}
