a7d8f007c5a7c3bcf9943d2dbf38bdd95f089dd1
choi-ik <ci980704@naver.com>
2025-02-06 16:41:46 +0900

    FLIZ-67 feat: 알림 관련 카테고리를 드로어 형식으로 나타낼 NotificationSideBar 컴포넌트 구현

---
+++ apps/trainer/components/NotificationSideBar.tsx
@@ -0,0 +1,34 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@ui/components/Drawer";
+import { Menu, X } from "lucide-react";
+
+import NotificationAccordion from "./NotificationAccordion";
+
+export default function NotificationSideBar() {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Menu className="text-text-primary" />
+      </DrawerTrigger>
+      <DrawerContent className="w-[20.313rem] py-[1.5rem]">
+        <DrawerHeader>
+          <DrawerTitle className="m-0">
+            <div className="text-title-1 flex items-center justify-between">
+              알림
+              <DrawerClose className="text-[20px]">
+                <X />
+              </DrawerClose>
+            </div>
+          </DrawerTitle>
+        </DrawerHeader>
+        <NotificationAccordion />
+      </DrawerContent>
+    </Drawer>
+  );
+}
