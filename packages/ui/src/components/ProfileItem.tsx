6fac0f01e8d8cf70c4b38ce33a1f7c64a590c92c
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-02-07 14:57:04 +0900

    FLIZ-77 feat: 사용자 ProfileItem 공통 컴포넌트 구현

---
+++ packages/ui/src/components/ProfileItem.tsx
@@ -0,0 +1,61 @@
+import { HTMLAttributes } from "react";
+
+import { cn } from "@ui/lib/utils";
+
+function ProfileItem({ className, children, ...props }: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        `bg-background-primary relative flex min-h-[2.813rem] min-w-[22.375rem] items-center`,
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+function ProfileItemIcon({ children, className, ...props }: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        `text-text-primary mr-[0.938rem] aspect-square min-h-[1.563rem] min-w-[1.563rem]`,
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+function ProfileItemTitle({ children, className, ...props }: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        `text-headline text-text-primary text-[1.063rem] leading-[1.375rem]`,
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+function ProfileItemContent({ children, className, ...props }: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        `text-text-sub2 grow-1 align-center absolute right-0 flex gap-[0.625rem] text-[0.938rem] leading-[1.375rem]`,
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+export { ProfileItem, ProfileItemIcon, ProfileItemTitle, ProfileItemContent };
