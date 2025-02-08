f48a8f098c8b3867b47113aba34b9564dc2ae657
yjc2021 <yongjae.choi20@gmail.com>
2025-02-08 15:48:03 +0900

    FLIZ-62 feat: ChevronTrigger 컴포넌트 구현

---
+++ packages/ui/src/components/ChevronTrigger.tsx
@@ -0,0 +1,32 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import React from "react";
+
+import { cn } from "@ui/lib/utils";
+
+const DEFAULT_ICON_SIZE = 24;
+
+type ChevronTriggerProps = React.ComponentProps<"div"> & {
+  position?: "left" | "right";
+  size?: number;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+};
+
+const ChevronTrigger = React.forwardRef<HTMLDivElement, ChevronTriggerProps>(
+  ({ onClick, children, className, position = "right", size = DEFAULT_ICON_SIZE }, ref) => (
+    <div
+      onClick={onClick}
+      className={cn(
+        "hover:bg-background-sub3 text-text-primary flex w-fit cursor-pointer items-center rounded-md transition-colors",
+        className,
+      )}
+      ref={ref}
+    >
+      {position === "left" && <ChevronLeft size={size} />}
+      {children}
+      {position === "right" && <ChevronRight size={size} />}
+    </div>
+  ),
+);
+ChevronTrigger.displayName = "ChevronTrigger";
+
+export default ChevronTrigger;
