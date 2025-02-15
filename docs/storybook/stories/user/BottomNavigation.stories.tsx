07aa40894c9635deea0e57db52133333d0a1afcf
MaSeungHyun <tpdlqj0514@gmail.com>
2025-02-15 19:12:14 +0900

    FLIZ-78 docs: 스토리북 decorator를 통한 render() 제거

---
+++ docs/storybook/stories/user/BottomNavigation.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from "@storybook/react";
+import BottomNavigation from "@user/components/BottomNavigation";
+
+const meta: Meta<typeof BottomNavigation> = {
+  component: BottomNavigation,
+  tags: ["autodocs"],
+  decorators: (Story) => (
+    <div className="h-[5.063rem] w-full">
+      <Story />
+    </div>
+  ),
+};
+
+export default meta;
+
+type BottomNavigationStory = StoryObj<typeof BottomNavigation>;
+
+export const Default: BottomNavigationStory = {};
