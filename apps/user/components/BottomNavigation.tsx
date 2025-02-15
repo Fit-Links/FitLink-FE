34363b9fbd08b785cf5ca5f4304505361548b90b
MaSeungHyun <tpdlqj0514@gmail.com>
2025-02-15 19:34:33 +0900

    FLIZ-78 feat: url State를 위한기존 네비게이터 내의 함수 제거

--- apps/user/components/BottomNavigation.tsx
+++ apps/user/components/BottomNavigation.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@ui/lib/utils";
 import { Bell, Calendar, UserRound } from "lucide-react";
-import React, { useState } from "react";
+import React from "react";
 
 type NavigationItemsProp = {
   label: string;
@@ -17,29 +17,20 @@ const NAVIGATION_ITEMS: NavigationItemsProp[] = [
 ];
 
 export default function BottomNavigation() {
-  const [page, setPage] = useState("");
-
-  const handleClickChangePage = (path: string) => {
-    setPage(path);
-  };
+  // TODO: rul 이동 로직 추가
 
   return (
     <nav className="bg-background-primary border-background-sub2 flex h-[5.063rem] w-full justify-around border-t">
-      {NAVIGATION_ITEMS.map(({ label, path, icon: Icon }) => (
+      {NAVIGATION_ITEMS.map(({ label, icon: Icon }) => (
         <button
           key={`navigation-${label}`}
           className={cn(
             "text-body-6 text-text-sub4 mt-[0.5rem] flex flex-1 flex-col items-center whitespace-nowrap",
-            page === path && "text-text-primary text-body-5",
           )}
-          onClick={() => handleClickChangePage(path)}
+          style={{}}
         >
           <Icon />
-          <div
-            className={cn("mt-[0.25rem]", page === path && "text-text-primary text-text-body-5")}
-          >
-            {label}
-          </div>
+          <div className={cn("mt-[0.25rem]")}>{label}</div>
         </button>
       ))}
     </nav>
