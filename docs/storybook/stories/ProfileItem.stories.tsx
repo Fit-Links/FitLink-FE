e297e9f5fbcb63f20c4e72cea2811ae663e67b92
MaSeungHyun <tpdlqj0514@gmail.com>
2025-02-15 18:28:08 +0900

    FLIZ-77 docs: 오브젝트 방식의 변경 구조에 따른 스토리북 재작성

--- docs/storybook/stories/ProfileItem.stories.tsx
+++ docs/storybook/stories/ProfileItem.stories.tsx
@@ -7,7 +7,7 @@ import {
 } from "@5unwan/ui/components/ProfileItem";
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { ChevronRight, User } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 
 const meta: Meta<typeof ProfileItem> = {
   component: ProfileItem,
@@ -18,61 +18,35 @@ export default meta;
 type Story = StoryObj<typeof ProfileItem>;
 
 export const Default: Story = {
-  render: () => (
-    <ProfileItem>
-      <ProfileItemHeader>
-        <ProfileItemIcon iconName="User" />
-        이름
-      </ProfileItemHeader>
-      <ProfileItemContent>홍길동</ProfileItemContent>
-    </ProfileItem>
-  ),
+  render: () => <ProfileItem variant="name">홍길동</ProfileItem>,
 };
 
 export const OneContent: Story = {
   render: () => (
-    <ProfileItem>
-      <ProfileItemHeader>
-        <ProfileItemIcon iconName="Code" />
-        트레이너 코드
-      </ProfileItemHeader>
-      <ProfileItemContent>
-        <div className="flex items-center">
-          <ChevronRight className="h-[1.563rem] w-[1.563rem]" />
-        </div>
-      </ProfileItemContent>
+    <ProfileItem variant="code">
+      <div className="flex items-center">
+        <ChevronRight className="h-[1.563rem] w-[1.563rem]" />
+      </div>
     </ProfileItem>
   ),
 };
 
 export const OneContent2: Story = {
   render: () => (
-    <ProfileItem>
-      <ProfileItemHeader>
-        <ProfileItemIcon iconName="Dumbbell" />
-        PT 횟수
-      </ProfileItemHeader>
-      <ProfileItemContent>
-        <Badge>00/20</Badge>
-      </ProfileItemContent>
+    <ProfileItem variant="dumbbell">
+      <Badge>00/20</Badge>
     </ProfileItem>
   ),
 };
 
 export const TwoContent: Story = {
   render: () => (
-    <ProfileItem>
-      <ProfileItemHeader>
-        <ProfileItemIcon iconName="Phone" />
-        전화번호
-      </ProfileItemHeader>
-      <ProfileItemContent>
-        010 1234 5678
-        <div className="text-text-sub4 flex text-[0.938rem] leading-[1.375rem]">
-          <div className="flex items-center">변경</div>
-          <ChevronRight className="h-[1.563rem] w-[1.563rem]" />
-        </div>
-      </ProfileItemContent>
+    <ProfileItem variant="phone">
+      010 1234 5678
+      <div className="text-text-sub4 flex text-[0.938rem] leading-[1.375rem]">
+        <div className="flex items-center">변경</div>
+        <ChevronRight className="h-[1.563rem] w-[1.563rem]" />
+      </div>
     </ProfileItem>
   ),
 };
