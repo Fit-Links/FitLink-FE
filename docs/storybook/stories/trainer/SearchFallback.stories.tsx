15661c03ce75f1326a014677da83238f37a381f0
MaSeungHyun <tpdlqj0514@gmail.com>
2025-02-17 02:17:16 +0900

    FLIZ-85 docs: decorator를 통한 render 함수 실행 제거

--- docs/storybook/stories/trainer/SearchFallback.stories.tsx
+++ docs/storybook/stories/trainer/SearchFallback.stories.tsx
@@ -5,18 +5,16 @@ import SearchFallback from "@trainer/components/SearchFallback";
 const meta: Meta<typeof SearchFallback> = {
   component: SearchFallback,
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div className="text-text-primary bg-background-primary flex h-screen w-screen flex-col items-center p-3">
+        <Story />
+      </div>
+    ),
+  ],
 };
 export default meta;
 
 type Story = StoryObj<typeof SearchFallback>;
 
-export const Default: Story = {
-  args: {
-    onClick: () => {},
-  },
-  render: (args) => (
-    <div className="text-text-primary bg-background-primary flex h-screen w-screen flex-col items-center p-3">
-      <SearchFallback {...args} />,
-    </div>
-  ),
-};
+export const Default: Story = {};
