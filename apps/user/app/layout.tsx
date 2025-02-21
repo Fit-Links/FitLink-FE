b60e37630cf1022d92602c16bc3472d5dffb1abf
Yongjae Choi <yongjae.choi20@gmail.com>
2025-02-21 11:49:48 +0900

    [FLIZ-98/user] style: 회원 도메인 기본 layout 스타일링 (#88)
    * FLIZ-98 chore: lucide-react 설치
    
    * FLIZ-98 rename: 불필요 .gitkeep 파일 제거
    
    * FLIZ-98 chore: tailwind content 경로에 @5unwan/ui 추가
    
    * FLIZ-98 chore: tailwind variable 연결
    
    * FLIZ-98 style: 기본 box-sizing 변경
    
    * FLIZ-98 chore: tailwind sizing 토큰에 mobile 추가
    
    * FLIZ-98 style: 프로젝트 기본 layout 스타일링
    
    * FLIZ-98 style: Header에 backgroundColor 및 height 스타일 추가

--- apps/user/app/layout.tsx
+++ apps/user/app/layout.tsx
@@ -1,11 +1,26 @@
+import { Viewport } from "next";
+
+import BottomNavigation from "@user/components/BottomNavigation";
+
+import "./global.css";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body className="bg-background-primary text-text-primary md:border-background-sub2 md:max-w-mobile relative box-content min-h-screen w-full md:mx-auto md:overflow-x-hidden md:border md:shadow-lg">
+        <div className="px-4 pb-[5.063rem]">{children}</div>
+        <BottomNavigation />
+      </body>
     </html>
   );
 }
