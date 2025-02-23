302e48f03e874da2fb61d378f2a332b367032457
MaSeungHyun <tpdlqj0514@gmail.com>
2025-02-24 03:16:38 +0900

    FLIZ-26 docs: Dialog를 활용한 Popup 컴포넌트 스토리북 작성

--- docs/storybook/stories/Popup.stories.tsx
+++ docs/storybook/stories/Popup.stories.tsx
@@ -1,59 +1,91 @@
+import { Button } from "@5unwan/ui/components/Button";
+import { PopupProps } from "@5unwan/ui/components/Popup";
 import type { Meta, StoryObj } from "@storybook/react";
-
-import { Popup, PopupButtonGroup, PopupContent, PopupTitle } from "@5unwan/ui/components/PopUp";
-import { cn } from "../../../packages/ui/src/lib/utils";
-import { useState } from "react";
-import { Badge } from "@5unwan/ui/components/Badge";
+import { Popup } from "@ui/components/Popup";
 
 const meta: Meta<typeof Popup> = {
   component: Popup,
   tags: ["autodocs"],
-  argTypes: {},
+
+  argTypes: {
+    title: {
+      control: "text",
+    },
+    description: {
+      control: "text",
+    },
+    positive: {
+      control: "object",
+    },
+    negative: {
+      control: "object",
+    },
+  },
 };
 export default meta;
 
 type Story = StoryObj<typeof Popup>;
 
 export const Default: Story = {
-  render: () => {
-    const [open, setOpen] = useState(false);
+  args: {
+    title: "트레이너와 연동이 필요합니다",
+    description: "연동 요청 시 본 서비스를 이용하실 수 있습니다",
+    positive: {
+      label: "연동 요청하기",
+      callback: () => {
+        console.log("연동 요청하기");
+      },
+    },
+  },
+  render: (args: PopupProps) => {
     return (
-      <div className="relative h-[300px] w-screen">
-        <Badge variant="brand" onClick={() => setOpen(true)}>
-          Open
-        </Badge>
-        <Popup open={open}>
-          <PopupContent container={document.querySelector("#story--popup--default--primary")}>
-            <PopupTitle>
-              예정되어 있는 PT 수업 시간을
-              <br />
-              삭제하시겠습니까?
-            </PopupTitle>
-            <PopupButtonGroup>
-              <div
-                className={cn(
-                  "text-headline focus-visible:ring-ring inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[10px] transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-                  "bg-background-sub1 text-text-primary hover:bg-background-sub2 shadow",
-                  "text-headline h-[2.5rem] px-[2.625rem]",
-                )}
-                onClick={() => setOpen(false)}
-              >
-                취소
-              </div>
-              <div
-                className={cn(
-                  "text-headline focus-visible:ring-ring inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[10px] transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-                  "bg-brand-primary-500 text-text-primary hover:bg-brand-primary-500/90 shadow",
-                  "text-headline h-[2.5rem] px-[2.625rem]",
-                )}
-                onClick={() => setOpen(false)}
-              >
-                삭제
-              </div>
-            </PopupButtonGroup>
-          </PopupContent>
-        </Popup>
-      </div>
+      <Popup {...args}>
+        <Button>Open</Button>
+      </Popup>
+    );
+  },
+};
+
+export const MultiLine: Story = {
+  args: {
+    title: "당일 변경은 PT 수업 시간의\\n1시간 전부터 가능합니다",
+    positive: {
+      label: "확인",
+      callback: () => {
+        console.log("확인");
+      },
+    },
+  },
+  render: (args: PopupProps) => {
+    return (
+      <Popup {...args}>
+        <Button>Open</Button>
+      </Popup>
+    );
+  },
+};
+
+export const TwoButton: Story = {
+  args: {
+    title: "대기중인 예약을 취소하시겠습니까?",
+    negative: {
+      label: "닫기",
+      callback: () => {
+        console.log("닫기");
+      },
+    },
+    positive: {
+      label: "확인",
+      callback: () => {
+        console.log("확인");
+      },
+    },
+  },
+  render: (args: PopupProps) => {
+    return (
+      <Popup {...args}>
+        <Button>Open</Button>
+      </Popup>
     );
   },
 };
