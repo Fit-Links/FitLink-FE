151f7f33e80f978859e4e83f001862b5a3be1f76
yjc2021 <yongjae.choi20@gmail.com>
2025-03-06 18:35:56 +0900

    FLIZ-115 feat: core api types에 추가 타입 정의

--- packages/core/src/api/types/common.ts
+++ packages/core/src/api/types/common.ts
@@ -30,3 +30,12 @@ export type AvailablePtTime = {
   startTime: string;
   endTime: string;
 };
+export type Gender = "MALE" | "FEMALE";
+export type BaseSignupInfo = {
+  name: string;
+  birthDate: string;
+  phoneNumber: string;
+  gender: Gender;
+  // TODO [2025.03.06]: BE 이미지 관리 정책 확정 후 업데이트
+  // profileUrl: string;
+};
