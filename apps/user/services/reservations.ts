de87c1f3dae27c9fdab4056240530a88e94b9e23
choi-ik <ci980704@naver.com>
2025-03-07 22:07:35 +0900

    FLIZ-117 chore: dto 타입 컨벤션 반영

--- apps/user/services/reservations.ts
+++ apps/user/services/reservations.ts
@@ -1,36 +1,37 @@
-import http from "@user/utils/apiClient";
-
+import http from "./core";
 import {
   CancelReservationApiResponse,
-  CancelReservationRequest,
+  CancelReservationRequestBody,
+  CancelReservationRequestPath,
   DirectReservationApiResponse,
-  DirectReservationRequest,
+  DirectReservationRequestBody,
   ReservationChangeApiResponse,
-  ReservationChangeRequest,
+  ReservationChangeRequestBody,
+  ReservationChangeRequestPath,
   ReservationDetailStatusApiResponse,
-  ReservationDetailStatusPathParmas,
+  ReservationDetailStatusRequestPath,
   ReservationStatusApiResponse,
-  ReservationStatusParams,
+  ReservationStatusRequestQuery,
 } from "./types/reservations";
 
 const RESERVATION_BASE_URL = "reservations";
 
 // 예약 현황 조회
-export const getReservationStatus = ({ date }: ReservationStatusParams) => {
+export const getReservationStatus = ({ date }: ReservationStatusRequestQuery) => {
   http.get<ReservationStatusApiResponse>({ url: `${RESERVATION_BASE_URL}`, params: { date } });
 };
 
 // 예약 상세 조회
-export const gerReservationDetailStatus = ({
+export const getReservationDetailStatus = ({
   reservationId,
-}: ReservationDetailStatusPathParmas) => {
+}: ReservationDetailStatusRequestPath) => {
   http.get<ReservationDetailStatusApiResponse>({
     url: `${RESERVATION_BASE_URL}/${reservationId}},`,
   });
 };
 
 // 직접 예약
-export const createDirectReservation = ({ reservations }: DirectReservationRequest) => {
+export const directReservation = ({ reservations }: DirectReservationRequestBody) => {
   http.post<DirectReservationApiResponse>({
     url: `${RESERVATION_BASE_URL}`,
     data: { reservations },
@@ -38,10 +39,13 @@ export const createDirectReservation = ({ reservations }: DirectReservationReque
 };
 
 // 예약 취소
-export const createCancelReservation = ({
-  reservationId,
-  cancel_reason,
-}: CancelReservationRequest) => {
+export const cancelReservation = (
+  requestPath: CancelReservationRequestPath,
+  requestBody: CancelReservationRequestBody,
+) => {
+  const { reservationId } = requestPath;
+  const { cancel_reason } = requestBody;
+
   http.post<CancelReservationApiResponse>({
     url: `${RESERVATION_BASE_URL}/${reservationId}}/cancel`,
     data: {
@@ -51,13 +55,13 @@ export const createCancelReservation = ({
 };
 
 // 예약 변경 요청
-export const createReservationChange = ({
-  reservationId,
-  reservationDate,
-  reservationDay,
-  changeDate,
-  changeDay,
-}: ReservationChangeRequest) => {
+export const reservationChange = (
+  requestPath: ReservationChangeRequestPath,
+  requestBody: ReservationChangeRequestBody,
+) => {
+  const { reservationId } = requestPath;
+  const { reservationDate, reservationDay, changeDate, changeDay } = requestBody;
+
   http.post<ReservationChangeApiResponse>({
     url: `${RESERVATION_BASE_URL}/${reservationId}`,
     data: {
--- apps/user/services/types/reservations.ts
+++ apps/user/services/types/reservations.ts
@@ -3,8 +3,6 @@ import {
   BaseReservationDetail,
   BaseReservationListItem,
   DayOfWeek,
-  ISODateString,
-  IsoDateTimeString,
   ReservationPathParams,
   ReservationStatus,
   ResponseBase,
@@ -12,20 +10,20 @@ import {
 
 /* 예약 현황 조회 */
 // 요청 파라미터: param
-export type ReservationStatusParams = {
-  date?: ISODateString;
+export type ReservationStatusRequestQuery = {
+  date?: string;
 };
 // 응답 데이터: 예약 항목 리스트
-export type ReservationStatusResponse = {
+type ReservationStatusResponse = {
   reservations: BaseReservationListItem[];
 };
 export type ReservationStatusApiResponse = ResponseBase<ReservationStatusResponse>;
 
 /* 예약 상세 조회  */
 // 요청 파라미터(path param)
-export type ReservationDetailStatusPathParmas = ReservationPathParams;
+export type ReservationDetailStatusRequestPath = ReservationPathParams;
 // 응답 데이터
-export type ReservationDetailStatusResponse = BaseReservationDetail<
+type ReservationDetailStatusResponse = BaseReservationDetail<
   Extract<ReservationStatus, "예약 확정">,
   BaseMemberInfo
 >;
@@ -33,17 +31,17 @@ export type ReservationDetailStatusApiResponse = ResponseBase<ReservationDetailS
 
 /* 직접 예약 */
 // 요청: Request Body
-export type DirectReservationRequest = {
+export type DirectReservationRequestBody = {
   reservations: {
     trainerId: number;
     memberId: number;
     name: string;
-    date: IsoDateTimeString[];
+    date: string[];
     priority: number;
   }[];
 };
 // 응답 데이터
-export type DirectReservationResponse = {
+type DirectReservationResponse = {
   reservation: {
     reservationId: number;
     status: Extract<ReservationStatus, "예약 대기">;
@@ -53,25 +51,27 @@ export type DirectReservationApiResponse = ResponseBase<DirectReservationRespons
 
 /* 예약 취소 */
 // 요청: 경로 변수와 Request Body 혼합
-export type CancelReservationRequest = ReservationPathParams & {
+export type CancelReservationRequestPath = ReservationPathParams;
+export type CancelReservationRequestBody = {
   cancel_reason: string;
 };
 // 응답 데이터
-export type CancelReservationResponse = {
+type CancelReservationResponse = {
   reservationId: number;
 };
 export type CancelReservationApiResponse = ResponseBase<CancelReservationResponse>;
 
 /* 예약 변경 요청 */
 // 요청: 경로 변수와 Request Body 혼합
-export type ReservationChangeRequest = ReservationPathParams & {
-  reservationDate: IsoDateTimeString;
+export type ReservationChangeRequestPath = ReservationPathParams;
+export type ReservationChangeRequestBody = {
+  reservationDate: string;
   reservationDay: DayOfWeek;
-  changeDate: ISODateString;
+  changeDate: string;
   changeDay: DayOfWeek;
 };
 // 응답 데이터
-export type ReservationChangeResponse = {
+type ReservationChangeResponse = {
   reservationId: number;
 };
 export type ReservationChangeApiResponse = ResponseBase<ReservationChangeResponse>;
