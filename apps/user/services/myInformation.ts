1c3c0eeaefb2b5e59ccc7c977e50f6fa3462f0e8
choi-ik <ci980704@naver.com>
2025-03-07 23:05:10 +0900

    FLIZ-133 chore: 유저 도메인 baseUrl 네이밍 수정

--- apps/user/services/myInformation.ts
+++ apps/user/services/myInformation.ts
@@ -14,12 +14,12 @@ import {
   MyPtHistoryRequestQuery,
 } from "./types/myInformation.dto";
 
-const USER_MANAGEMENT_BASE_URL = "members";
+const USER_BASE_URL = "members";
 
 /** 트레이너 연결 요청 */
 export const connectTrainer = ({ trainerCode }: ConnectTrainerRequestBody) => {
   http.post<ConnectTrainerApiResponse>({
-    url: `${USER_MANAGEMENT_BASE_URL}/connect`,
+    url: `${USER_BASE_URL}/connect`,
     data: {
       trainerCode,
     },
@@ -28,31 +28,31 @@ export const connectTrainer = ({ trainerCode }: ConnectTrainerRequestBody) => {
 
 /** 트레이너 연결 해제 요청 */
 export const disconnectTrainer = () => {
-  http.post<DisconnectTrainerApiResponse>({ url: `${USER_MANAGEMENT_BASE_URL}/disconnect` });
+  http.post<DisconnectTrainerApiResponse>({ url: `${USER_BASE_URL}/disconnect` });
 };
 
 /** 내 정보 조회 */
 export const myInformation = () => {
-  http.get<MyInformationApiResponse>({ url: `${USER_MANAGEMENT_BASE_URL}/me` });
+  http.get<MyInformationApiResponse>({ url: `${USER_BASE_URL}/me` });
 };
 
 /** 내 정보 수정 */
 export const editMyInformation = ({ name, phoneNumber }: EditMyInformationRequestBody) => {
   http.patch<EditMyInformationApiResponse>({
-    url: `${USER_MANAGEMENT_BASE_URL}/me`,
+    url: `${USER_BASE_URL}/me`,
     data: { name, phoneNumber },
   });
 };
 
 /** 내 정보 상세 조회 */
 export const myInformationDetail = () => {
-  http.get<MyInformationDetailApiResponse>({ url: `${USER_MANAGEMENT_BASE_URL}/me/detail` });
+  http.get<MyInformationDetailApiResponse>({ url: `${USER_BASE_URL}/me/detail` });
 };
 
 /** PT 희망시간 수정 */
 export const editPreferredTime = (requestBody: EditPreferredTimeRequestBody) => {
   http.put<EditPreferredTimeApiResponse>({
-    url: `${USER_MANAGEMENT_BASE_URL}/me/workout-schedult`,
+    url: `${USER_BASE_URL}/me/workout-schedult`,
     data: requestBody,
   });
 };
@@ -66,7 +66,7 @@ export const myPtHistory = (
   const { status, size, page } = requestQuery;
 
   http.get<MyPtHistoryApiResponse>({
-    url: `${USER_MANAGEMENT_BASE_URL}/${memberId}/sessions`,
+    url: `${USER_BASE_URL}/${memberId}/sessions`,
     params: { status, size, page },
   });
 };
