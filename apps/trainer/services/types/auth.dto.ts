b4f1ca38521b97b1dd15298f17dc3477a8e94fff
Yongjae Choi <yongjae.choi20@gmail.com>
2025-03-07 23:21:24 +0900

    [FLIZ-131/trainer] feat: Auth DTO 및 API 작업 (#98)
    * FLIZ-131 rename: 불필요 .gitkeep 파일 제거
    
    * FLIZ-131 rename: user 도메인 불필요 auth 타입 제거
    
    * FLIZ-131 feat: trainer 서비스 auth 도메인 DTO 타입 정의
    
    * FLIZ-131 feat: trainer 서비스 auth 도메인 API 요청 함수 정의

--- apps/trainer/services/types/auth.dto.ts
+++ apps/trainer/services/types/auth.dto.ts
@@ -1,12 +1,12 @@
 import {
+  AvailablePtTime,
   BaseSignupInfo,
   NoResponseData,
-  PreferredWorkout,
   ResponseBase,
 } from "@5unwan/core/api/types/common";
 
 export type SignupRequestBody = BaseSignupInfo & {
-  workoutSchedule: PreferredWorkout[];
+  availableTimes: Omit<AvailablePtTime, "availableTimeId">[];
 };
 
 export type SignupApiResponse = ResponseBase<{ accessToken: string; refreshToken: string }>;
