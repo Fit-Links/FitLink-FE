70f65e35a4737b2516128ff468453d695b6a2a96
yjc2021 <yongjae.choi20@gmail.com>
2025-03-07 21:12:19 +0900

    FLIZ-131 rename: user 도메인 불필요 auth 타입 제거

--- apps/user/services/types/auth.dto.ts
+++ apps/user/services/types/auth.dto.ts
@@ -1,17 +1,9 @@
 import {
   BaseSignupInfo,
-  DayOfWeek,
   NoResponseData,
   PreferredWorkout,
   ResponseBase,
 } from "@5unwan/core/api/types/common";
-
-export type AvailablePtTime = {
-  dayOfWeek: DayOfWeek;
-  isHoliday: boolean;
-  startTime: string;
-  endTime: string;
-};
 
 export type SignupRequestBody = BaseSignupInfo & {
   workoutSchedule: PreferredWorkout[];
