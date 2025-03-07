67a573ee36a7d69490dafa74cc86d6b1efd9eff2
Yongjae Choi <yongjae.choi20@gmail.com>
2025-03-07 23:21:36 +0900

    [FLIZ-132/trainer] feat: 알림 DTO 및 API 작업 (#99)
    * FLIZ-132 rename: 불필요 .gitkeep 파일 제거
    
    * FLIZ-132 feat: trainer 서비스 알림 도메인 DTO 타입 정의
    
    * FLIZ-132 feat: trainer 서비스 알림 도메인 API 요청 함수 정의
    
    * FLIZ-132 fix: user 서비스 알림 도메인 API 요청 경로 수정

--- apps/trainer/services/notification.ts
+++ apps/trainer/services/notification.ts
@@ -2,16 +2,15 @@ import http from "@5unwan/core/api/core";
 
 import {
   GetNotificationApiResponse,
+  GetNotificationRequestQuery,
   ReadNotificationApiResponse,
   ReadNotificationRequestBody,
 } from "./types/notification.dto";
 
-export const getNotification = () =>
+export const getNotification = (params: GetNotificationRequestQuery) =>
   http.get<GetNotificationApiResponse>({
     url: `/v1/notifications`,
-    params: {
-      type: "all",
-    },
+    params,
   });
 
 export const readNotification = (data: ReadNotificationRequestBody) =>
