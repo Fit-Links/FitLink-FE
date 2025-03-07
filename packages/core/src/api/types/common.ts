09434e1c18c5bae19550083a2a0af41b98515357
Ik <ci980704@gmail.com>
2025-03-07 14:53:24 +0900

    [FILZ-114/trainer] feat: 트레이너 도메인 reservation DTO 타입 정의 및 API 함수 구현 (#92)
    * FLIZ-114 chore: packages/core에 DTO 공통 타입 정의
    
    * FLIZ-114 chore: 트레이너 도메인 reservation 타입 정의
    
    * FLIZ-114 feat: 트레이너 도메인 reservation api 함수 구현
    
    * FLIZ-114 feat: 트레이너 도메인에서 api core 로직을 사용할 수 있도록 baseurl과 token provider를 주입하는  apiClient 유틸함수 구현
    
    * FLIZ-114 chore: apps/trainer의 package.json에 packages/core 의존성 주입
    
    * FLIZ-114 chore: 유틸리티 타입 문법 에러 수정
    
    * FLIZ-114 rename: apiClient의 파일명을 core로 변경 및 파일 위치를 service로 변경
    
    * FLIZ-114 chore: 날짜 데이터 관련 타입을 string으로 모두 변경
    
    * FLIZ-114 chore: response data 속성 타입 export 제거
    
    * FLIZ-114 chore: api request dto 타입의 네이밍 컨벤션 적용 및 api 함수에도 수정사항 반영

--- packages/core/src/api/types/common.ts
+++ packages/core/src/api/types/common.ts
@@ -30,3 +30,47 @@ export type AvailablePtTime = {
   startTime: string;
   endTime: string;
 };
+
+// 예약 상태 (한글 리터럴)
+export type ReservationStatus = "예약 확정" | "예약 대기" | "예약 불가" | "수업 완료" | "휴뮤일";
+
+// 회원 정보 (기본)
+export type BaseMemberInfo = {
+  memberId: number;
+  name: string;
+};
+
+// 트레이너 등에서 사용하는 상세 회원 정보
+export type DetailedMemberInfo = BaseMemberInfo & {
+  birthdate: string;
+  phoneNumber: string;
+  profilePictureUrl: string;
+};
+
+// 공통 예약 리스트 항목 (트레이너, 유저 모두 사용)
+export type BaseReservationListItem = {
+  reservationId: number;
+  sessionInfoId: number;
+  isDayOff: boolean;
+  dayOfWeek: DayOfWeek;
+  reservationDate: string;
+  status: ReservationStatus;
+  memberInfo: BaseMemberInfo;
+};
+
+// 공통 예약 상세 타입 (예약 상세 조회 시)
+export type BaseReservationDetail<
+  TStatus extends ReservationStatus = ReservationStatus,
+  TMember = BaseMemberInfo,
+> = {
+  dayOfWeek: DayOfWeek;
+  reservationDate: string;
+  sessionId: number;
+  status: TStatus;
+  memberInfo: TMember;
+};
+
+// 경로(Path) 변수에 사용되는 공통 타입
+export type ReservationPathParams = {
+  reservationId: number;
+};
