3f445be5779c34d9eec9c4b1f6212a3df6833c4c
yjc2021 <yongjae.choi20@gmail.com>
2025-03-09 15:58:42 +0900

    FLIZ-136 feat: reservations 도메인 쿼리키 정의

---
+++ apps/user/hooks/reservations/queries.ts
@@ -0,0 +1,19 @@
+import { queryOptions } from "@tanstack/react-query";
+
+import { getReservationDetailStatus, getReservationStatus } from "@user/services/reservations";
+
+const reservationQueries = {
+  all: () => ["reservations"],
+  lists: () => [...reservationQueries.all(), "list"],
+  list: (date?: string) =>
+    queryOptions({
+      queryKey: [...reservationQueries.lists(), date],
+      queryFn: () => getReservationStatus({ date }),
+    }),
+  details: () => [...reservationQueries.all(), "detail"],
+  detail: (reservationId: number) =>
+    queryOptions({
+      queryKey: [...reservationQueries.details(), reservationId],
+      queryFn: () => getReservationDetailStatus({ reservationId }),
+    }),
+};
