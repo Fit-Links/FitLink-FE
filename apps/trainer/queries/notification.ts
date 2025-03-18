84739f91d746cbf7713edb740ae5ca2887cd6904
choi-ik <ci980704@naver.com>
2025-03-18 17:10:59 +0900

    FLIZ-137 feat: 트레이너 도메인 알림 관련 쿼리 팩토리 구현

---
+++ apps/trainer/queries/notification.ts
@@ -0,0 +1,16 @@
+import { NotificationType } from "@5unwan/core/api/types/common";
+import { queryOptions } from "@tanstack/react-query";
+
+import { getNotification } from "@trainer/services/notification";
+
+export const notificationBaseKeys = {
+  all: ["notification"] as const,
+};
+
+export const notificationQueries = {
+  notifications: (type: NotificationType, name?: string) =>
+    queryOptions({
+      queryKey: [...notificationBaseKeys.all, type, name],
+      queryFn: () => getNotification({ type, name }),
+    }),
+};
