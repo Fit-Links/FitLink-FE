48aec3b85f56bf0a6fe240d973cabdf9577e4299
yjc2021 <yongjae.choi20@gmail.com>
2025-03-24 11:54:39 +0900

    FLIZ-151 feat: ResultStep 구현

---
+++ apps/user/components/ResultStep.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { Button } from "@ui/components/Button";
+import RequestStatus, { Status } from "@ui/components/RequestStatus";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+import { SignupRequestBody } from "@user/services/types/auth.dto";
+
+import { useSignupForm } from "@user/hooks/useSignupForm";
+
+type ResultStepProps = {
+  form: SignupRequestBody;
+};
+function ResultStep({ form }: ResultStepProps) {
+  const isInitializedRef = React.useRef(false);
+  const [status, setStatus] = React.useState<Status>("pending");
+
+  const { onSubmit } = useSignupForm();
+  const router = useRouter();
+
+  const handleClick = (status: "success" | "error") => {
+    if (status === "success") {
+      router.replace("/");
+    } else if (status === "error") {
+      setStatus("pending");
+      onSubmit(form, {
+        onSuccess: () => setStatus("success"),
+        onError: () => setStatus("error"),
+      });
+    }
+  };
+  React.useEffect(() => {
+    if (isInitializedRef.current) return;
+
+    isInitializedRef.current = true;
+
+    onSubmit(form, {
+      onSuccess: () => setStatus("success"),
+      onError: () => setStatus("error"),
+    });
+  }, []);
+
+  return (
+    <div className="flex h-full flex-col">
+      <div className="flex flex-1 items-center justify-center border border-white">
+        <RequestStatus
+          status={status}
+          contentPerStatus={{
+            success: {
+              title: "회원가입이 완료되었습니다",
+            },
+            error: {
+              title: "회원가입에 실패했습니다",
+              description: "네트워크 연결 상태를 확인하고 다시 시도해주세요",
+            },
+          }}
+        />
+      </div>
+      <div className="h-[3.375rem] w-full">
+        <SignupButton status={status} onClick={handleClick}></SignupButton>
+      </div>
+    </div>
+  );
+}
+
+export default ResultStep;
+
+type SignupButtonProps = {
+  status: Status;
+  onClick: (status: "success" | "error") => void;
+};
+function SignupButton({ status, onClick }: SignupButtonProps) {
+  if (status === "pending") return <></>;
+
+  return (
+    <Button size="xl" className="w-full" onClick={() => onClick(status)}>
+      {status === "success" ? "홈으로 가기" : "재시도 하기"}
+    </Button>
+  );
+}
