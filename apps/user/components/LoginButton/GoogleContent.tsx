9e92e4f34253d9da7cf082e7fd59b8b0d929c8f8
yjc2021 <yongjae.choi20@gmail.com>
2025-03-25 14:19:51 +0900

    FLIZ-151 refactor: LoginButton 스타일 토큰 상수로 분리

--- apps/user/components/LoginButton/GoogleContent.tsx
+++ apps/user/components/LoginButton/GoogleContent.tsx
@@ -1,11 +1,19 @@
+import { cn } from "@ui/lib/utils";
 import Image from "next/image";
 import React from "react";
+
+import { OAUTH_LOGO_SIZE, OAUTH_TEXT_SIZE_STYLE } from "@user/constants/loginButton";
 
 function GoogleContent() {
   return (
     <div className="text-headline flex h-full items-center justify-center gap-2 bg-[#E9E9E9] font-semibold text-[#1C1C1C]">
-      <Image src="/google_logo.png" alt="구글 로그인" height={15} width={15} />
-      <span>구글 로그인</span>
+      <Image
+        src="/google_logo.png"
+        alt="구글 로그인"
+        height={OAUTH_LOGO_SIZE}
+        width={OAUTH_LOGO_SIZE}
+      />
+      <span className={cn(OAUTH_TEXT_SIZE_STYLE)}>구글 로그인</span>
     </div>
   );
 }
--- apps/user/components/LoginButton/KakaoContent.tsx
+++ apps/user/components/LoginButton/KakaoContent.tsx
@@ -1,11 +1,19 @@
+import { cn } from "@ui/lib/utils";
 import Image from "next/image";
 import React from "react";
+
+import { OAUTH_LOGO_SIZE, OAUTH_TEXT_SIZE_STYLE } from "@user/constants/loginButton";
 
 function KakaoContent() {
   return (
     <div className="text-headline flex h-full items-center justify-center gap-2 bg-[#FDDC3F] font-semibold text-[#3A2929]">
-      <Image src="/kakao_logo.png" alt="카카오 로그인" height={15} width={15} />
-      <span>카카오 로그인</span>
+      <Image
+        src="/kakao_logo.png"
+        alt="카카오 로그인"
+        height={OAUTH_LOGO_SIZE}
+        width={OAUTH_LOGO_SIZE}
+      />
+      <span className={cn(OAUTH_TEXT_SIZE_STYLE)}>카카오 로그인</span>
     </div>
   );
 }
