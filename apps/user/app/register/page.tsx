bcd00fc9abee8f4763b174faf2d3d5fd08ba3c7f
yjc2021 <yongjae.choi20@gmail.com>
2025-03-26 11:30:42 +0900

    FLIZ-151 refactor: register 페이지 main 태그 추가

--- apps/user/app/register/page.tsx
+++ apps/user/app/register/page.tsx
@@ -1,17 +1,15 @@
 import dynamic from "next/dynamic";
 import React from "react";
 
-import TokenProvider from "@user/components/Providers/TokenProvider";
-
 const RegisterFunnel = dynamic(() => import("./_components/RegisterFunnel"), {
   ssr: false,
 });
 
 function RegisterPage() {
   return (
-    <TokenProvider>
+    <main className="h-full w-full">
       <RegisterFunnel />
-    </TokenProvider>
+    </main>
   );
 }
 
