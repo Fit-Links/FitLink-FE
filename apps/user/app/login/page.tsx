23520e7ea3eb15119e0f1029dd143178d0e9d7f8
yjc2021 <yongjae.choi20@gmail.com>
2025-03-26 10:57:27 +0900

    FLIZ-151 chore: 변경된 LoginButton import 경로 적용

--- apps/user/app/login/page.tsx
+++ apps/user/app/login/page.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 
-import LoginButton from "@user/components/LoginButton";
-import GoogleContent from "@user/components/LoginButton/GoogleContent";
-import KakaoContent from "@user/components/LoginButton/KakaoContent";
-import NaverContent from "@user/components/LoginButton/NaverContent";
+import LoginButton from "./_components/LoginButton";
+import GoogleContent from "./_components/LoginButton/GoogleContent";
+import KakaoContent from "./_components/LoginButton/KakaoContent";
+import NaverContent from "./_components/LoginButton/NaverContent";
 
 function LoginPage() {
   return (
-    <div className="flex h-full flex-col justify-between pb-[3.375rem] pt-[6.25rem]">
-      <main>
+    <main className="flex h-full flex-col justify-between pb-[3.375rem] pt-[6.25rem]">
+      <section>
         <h1 className="text-[1.6875rem] font-medium">
           가장 편한 방법으로 <br />
           시작해보세요!
         </h1>
         <h3 className="text-body-1">1분이면 회원가입이 가능해요</h3>
-      </main>
+      </section>
       <div className="flex flex-col items-center gap-4">
         <LoginButton type="kakao" renderContent={KakaoContent} />
         <LoginButton type="naver" renderContent={NaverContent} />
         <LoginButton type="google" renderContent={GoogleContent} />
       </div>
-    </div>
+    </main>
   );
 }
 
