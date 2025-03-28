286f28dda8d56cd39fad68f8be70d6dde0c2f9c8
choi-ik <ci980704@naver.com>
2025-03-28 19:13:19 +0900

    FLIZ-121 feat: 일정관리/예약 페이지의 root 페이지 구현

---
+++ apps/user/app/schedule-management/page.tsx
@@ -0,0 +1,16 @@
+import AddReservation from "./_components/AddReservation";
+import Calendar from "./_components/Calendar";
+
+function ScheduleManagement() {
+  /** TODO: 트레이너 연동 인증 로직으로 구현 */
+
+  return (
+    <main className="relative flex h-full w-full justify-center overflow-y-auto">
+      {/* <Link href={"http://3.34.169.105/oauth2/authorization/kakao"}>카카오 로그인</Link> */}
+      <Calendar />
+      <AddReservation trainerConnectStatus="CONNECTED" ptCount={1} />
+    </main>
+  );
+}
+
+export default ScheduleManagement;
