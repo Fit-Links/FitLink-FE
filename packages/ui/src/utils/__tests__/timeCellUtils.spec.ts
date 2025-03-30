ca0e879230bf6a090f2b0ba58d368b0b1dd9f112
yjc2021 <yongjae.choi20@gmail.com>
2025-03-30 13:17:46 +0900

    FLIZ-151 fix: 실패한 test code 수정

--- packages/ui/src/utils/__tests__/timeCellUtils.spec.ts
+++ packages/ui/src/utils/__tests__/timeCellUtils.spec.ts
@@ -4,11 +4,11 @@ import { splitTimeCellByAMPM, TimeCell } from "@ui/utils/timeCellUtils";
 describe("splitTimeCellByAMPM", () => {
   it("자정과 정오를 기준으로 time cell을 AM, PM으로 분류한다", () => {
     const timeCells: TimeCell[] = [
-      { dayOfWeek: "MON", time: "09:00", disabled: false },
-      { dayOfWeek: "TUE", time: "12:30", disabled: false },
-      { dayOfWeek: "WED", time: "15:00", disabled: true },
-      { dayOfWeek: "THU", time: "05:00", disabled: false },
-      { dayOfWeek: "FRI", time: "23:59", disabled: false },
+      { dayOfWeek: "MONDAY", time: "09:00", disabled: false },
+      { dayOfWeek: "TUESDAY", time: "12:30", disabled: false },
+      { dayOfWeek: "WEDNESDAY", time: "15:00", disabled: true },
+      { dayOfWeek: "THURSDAY", time: "05:00", disabled: false },
+      { dayOfWeek: "FRIDAY", time: "23:59", disabled: false },
     ];
 
     const { am, pm } = splitTimeCellByAMPM(timeCells);
@@ -16,42 +16,42 @@ describe("splitTimeCellByAMPM", () => {
     expect(am).toHaveLength(2);
     expect(am).toEqual(
       expect.arrayContaining([
-        { dayOfWeek: "MON", time: "09:00", disabled: false },
-        { dayOfWeek: "THU", time: "05:00", disabled: false },
+        { dayOfWeek: "MONDAY", time: "09:00", disabled: false },
+        { dayOfWeek: "THURSDAY", time: "05:00", disabled: false },
       ]),
     );
 
     expect(pm).toHaveLength(3);
     expect(pm).toEqual(
       expect.arrayContaining([
-        { dayOfWeek: "TUE", time: "12:30", disabled: false },
-        { dayOfWeek: "WED", time: "15:00", disabled: true },
-        { dayOfWeek: "FRI", time: "23:59", disabled: false },
+        { dayOfWeek: "TUESDAY", time: "12:30", disabled: false },
+        { dayOfWeek: "WEDNESDAY", time: "15:00", disabled: true },
+        { dayOfWeek: "FRIDAY", time: "23:59", disabled: false },
       ]),
     );
   });
 
   it("자정(00:00)과 정오(12:00)에 대한 엣지 케이스를 제대로 처리해야 한다", () => {
     const timeCells: TimeCell[] = [
-      { dayOfWeek: "MON", time: "00:00", disabled: false },
-      { dayOfWeek: "TUE", time: "12:00", disabled: false },
-      { dayOfWeek: "WED", time: "00:01", disabled: false },
-      { dayOfWeek: "THU", time: "12:01", disabled: false },
+      { dayOfWeek: "MONDAY", time: "00:00", disabled: false },
+      { dayOfWeek: "TUESDAY", time: "12:00", disabled: false },
+      { dayOfWeek: "WEDNESDAY", time: "00:01", disabled: false },
+      { dayOfWeek: "THURSDAY", time: "12:01", disabled: false },
     ];
 
     const { am, pm } = splitTimeCellByAMPM(timeCells);
 
     expect(am).toEqual(
       expect.arrayContaining([
-        { dayOfWeek: "MON", time: "00:00", disabled: false },
-        { dayOfWeek: "WED", time: "00:01", disabled: false },
+        { dayOfWeek: "MONDAY", time: "00:00", disabled: false },
+        { dayOfWeek: "WEDNESDAY", time: "00:01", disabled: false },
       ]),
     );
 
     expect(pm).toEqual(
       expect.arrayContaining([
-        { dayOfWeek: "TUE", time: "12:00", disabled: false },
-        { dayOfWeek: "THU", time: "12:01", disabled: false },
+        { dayOfWeek: "TUESDAY", time: "12:00", disabled: false },
+        { dayOfWeek: "THURSDAY", time: "12:01", disabled: false },
       ]),
     );
   });
