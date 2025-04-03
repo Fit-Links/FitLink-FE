75b2926aa74a88492fb4fa2a870e1b66e01f8bb9
choi-ik <ci980704@naver.com>
2025-04-04 00:11:42 +0900

    FLIZ-170 feat: 유저 프로필 카드 및 프로필 카드 내 유저의 운동 가능 시간을 나타내는 드랍다운 구현

---
+++ apps/trainer/app/schedule-management/reservation/_components/MemberListContainer/MemberWorkoutSchedule.tsx
@@ -0,0 +1,48 @@
+import { Dropdown, DropdownContent, DropdownItem, DropdownTrigger } from "@ui/components/Dropdown";
+import React from "react";
+
+type MemberWorkoutScheduleProps = {
+  memberId: number;
+};
+
+/** MemberID를 바탕으로 회원의 상세 정보를 가져와 회원의 운동 가능한 시간 드랍다운으로 나타내기 */
+function MemberWorkoutSchedule({ memberId }: MemberWorkoutScheduleProps) {
+  const workoutSchedules = MOCK_MEMBER_SCHEDULE[memberId];
+
+  return (
+    <Dropdown>
+      <DropdownTrigger>{`${workoutSchedules[0].day} ${workoutSchedules[0].hours}`}</DropdownTrigger>
+      <DropdownContent>
+        {workoutSchedules.map(({ day, hours }) => (
+          <DropdownItem key={day} className="flex gap-2">
+            <span>{day}</span>
+            <span>{hours}</span>
+          </DropdownItem>
+        ))}
+      </DropdownContent>
+    </Dropdown>
+  );
+}
+
+export default MemberWorkoutSchedule;
+
+const MOCK_MEMBER_SCHEDULE: { [key: number]: { day: string; hours: string }[] } = {
+  1: [
+    { day: "월", hours: "09:00 - 23:00" },
+    { day: "화", hours: "12:00 - 12:00" },
+    { day: "수", hours: "12:00 - 12:00" },
+    { day: "목", hours: "12:00 - 12:00" },
+    { day: "금", hours: "12:00 - 12:00" },
+    { day: "토", hours: "12:00 - 12:00" },
+    { day: "일", hours: "12:00 - 12:00" },
+  ],
+  2: [
+    { day: "월", hours: "09:00 - 23:00" },
+    { day: "화", hours: "12:00 - 12:00" },
+    { day: "수", hours: "12:00 - 12:00" },
+    { day: "목", hours: "12:00 - 12:00" },
+    { day: "금", hours: "12:00 - 12:00" },
+    { day: "토", hours: "12:00 - 12:00" },
+    { day: "일", hours: "12:00 - 12:00" },
+  ],
+};
