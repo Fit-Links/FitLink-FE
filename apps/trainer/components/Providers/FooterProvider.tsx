e156d0726f0372e860146543d925501afeacdde8
choi-ik <ci980704@naver.com>
2025-04-03 14:33:21 +0900

    FLIZ-170 feat: Layout 및 QueryClient, 바텀 네비게이션 Provider 구현

---
+++ apps/trainer/components/Providers/FooterProvider.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { cn } from "@ui/lib/utils";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+import BottomNavigation from "../BottomNavigation";
+
+// TODO[2025.03.18]: user 모든 경로 분류하기
+const PATHS = {
+  WITH_FOOTER: new Set(["/", "/schedule-management"]),
+  WITHOUT_FOOTER: new Set(["/register", "/login"]),
+};
+
+const doesPathNeedFooter = (pathName: string) => PATHS.WITH_FOOTER.has(pathName);
+
+function FooterProvider({ children }: { children: React.ReactNode }) {
+  const pathName = usePathname();
+
+  const hasFooter = doesPathNeedFooter(pathName);
+
+  return (
+    <>
+      <div
+        className={cn("h-full px-4", {
+          "pb-[5.063rem]": hasFooter,
+          "pb-[2.125rem]": !hasFooter,
+        })}
+      >
+        {children}
+      </div>
+      {hasFooter && <BottomNavigation />}
+    </>
+  );
+}
+
+export default FooterProvider;
