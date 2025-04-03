2b619e6af0429c06e1257b0292f51be73307e3ec
MaSeungHyun <tpdlqj0514@gmail.com>
2025-04-04 03:02:48 +0900

    FLIZ-126 feat: 내 트레이너 정보 페이지 컴포넌트화 및 구조 변경

---
+++ apps/user/app/my-page/my-trainer-information/_components/BottomSheet/UnlinkAlarmSheet.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@ui/components/Button";
+import Icon from "@ui/components/Icon";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@ui/components/Sheet";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface UnlinkAlarmSheetProps {
+  isOpenBottomSheet: boolean;
+  setIsOpenBottomSheet: (isOpenBottomSheet: boolean) => void;
+}
+
+export default function UnlinkAlarmSheet({
+  isOpenBottomSheet,
+  setIsOpenBottomSheet,
+}: UnlinkAlarmSheetProps) {
+  const router = useRouter();
+
+  const handleClickSheetClose = () => {
+    router.back();
+  };
+
+  return (
+    <Sheet open={isOpenBottomSheet} onOpenChange={setIsOpenBottomSheet}>
+      <SheetTrigger></SheetTrigger>
+      <SheetContent side={"bottom"}>
+        <SheetHeader>
+          <SheetTitle className="flex justify-center">
+            <Icon name="Check" className="h-[3.125rem] w-[3.125rem]" background="brand" />
+          </SheetTitle>
+        </SheetHeader>
+        <SheetDescription asChild>
+          <div className="flex flex-col gap-[0.875rem] text-center">
+            <p className="text-title-1">트레이너와 연동 해제되었습니다</p>
+            <p className="text-body-1">트레이너에게 연동 해제 알람이 전송돼요</p>
+          </div>
+        </SheetDescription>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button variant={"brand"} className="h-[2.5rem] w-full" onClick={handleClickSheetClose}>
+              확인
+            </Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}
---
+++ apps/user/app/my-page/my-trainer-information/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@ui/components/Avatar";
+import { ProfileItem } from "@ui/components/ProfileItem";
+import React, { useState } from "react";
+
+import { MyInformationDetailApiResponse } from "@user/services/types/myInformation.dto";
+
+import Header from "../_components/Header";
+import TrainerUnlinkItem from "./_components/TrainerUnlinkItem";
+
+export default function MyTrainerInformation() {
+  const [mockData] = useState<MyInformationDetailApiResponse["data"]>({
+    memberId: 2,
+    name: "김민수",
+    birthDate: "1990-05-12",
+    phoneNumber: "010-2938-2312",
+    profilePictureUrl: "https://github.com/shadcn.png",
+  });
+
+  return (
+    <main className="flex h-screen w-full flex-col items-center overflow-hidden pb-[5.063rem]">
+      <Header title="트레이너" />
+
+      <Avatar className="mt-[1.563rem]  h-[6.313rem] w-[6.313rem]">
+        <AvatarFallback />
+        <AvatarImage src={mockData.profilePictureUrl} />
+      </Avatar>
+
+      <ProfileItem className="w-full" variant={"name"}>
+        {mockData.name}
+      </ProfileItem>
+      <ProfileItem className="w-full" variant={"phone"}>
+        {mockData.phoneNumber}
+      </ProfileItem>
+
+      <TrainerUnlinkItem />
+    </main>
+  );
+}
