fdfcfba6508e7703a3e5413b15cedd06b12a353a
MaSeungHyun <tpdlqj0514@gmail.com>
2025-04-04 02:50:45 +0900

    FLIZ-126 feat: 마이페이지 메인 컴포넌트화 및 구조 변경

--- apps/user/app/my-page/page.tsx
+++ apps/user/app/my-page/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import { PtInfo } from "@5unwan/core/api/types/common";
 import { Badge } from "@ui/components/Badge";
-import { Button } from "@ui/components/Button";
-import { Dropdown, DropdownContent, DropdownItem, DropdownTrigger } from "@ui/components/Dropdown";
-import { Popup, PopupTrigger } from "@ui/components/Popup";
-import ProfileHeader from "@ui/components/ProfileHeader";
-import { ProfileItem, ProfileItemContent } from "@ui/components/ProfileItem";
-import PTHistoryItem from "@ui/components/PTHistoryItem";
-import { ChevronRight } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { ProfileItem } from "@ui/components/ProfileItem";
 import { useState } from "react";
 
 import {
@@ -19,20 +11,14 @@ import {
 
 import { getFormattedPTCount } from "@user/utils/count";
 
-type PTHistoryFilterTypes = "ALL" | "COMPLETED" | "NO_SHOW" | "NONE";
+import ProfileHeader from "./_components/ProfileHeader";
+import PTHistoryContent from "./_components/PTHistory/PTHistoryContent";
+import PTHistoryFilter from "./_components/PTHistory/PTHistoryFilter";
+import PTHistoryProvider from "./_components/PTHistory/PTHistoryProvider";
+import ConnectedTrainerItem from "./_components/PTInformation/ConnectedTrainerItem";
+import ScheduleInformationItem from "./_components/PTInformation/ScheduleInformationItem";
 
-export default function Home() {
-  const router = useRouter();
-
-  const FILTER_OPTIONS: Record<PTHistoryFilterTypes, string> = {
-    ALL: "전체",
-    COMPLETED: "PT 완료",
-    NO_SHOW: "불참석",
-    NONE: "미처리",
-  };
-
-  const [ptHistoryFilter, setPtHistoryFilter] = useState<PTHistoryFilterTypes>("ALL");
-
+export default function MyPage() {
   const [mockData] = useState<MyInformationApiResponse["data"]>({
     memberId: 1,
     name: "홍길동",
@@ -84,7 +70,7 @@ export default function Home() {
     ],
   });
 
-  const [mock_pt_history] = useState<MyPtHistoryApiResponse["data"]>({
+  const [ptHistory] = useState<MyPtHistoryApiResponse["data"]>({
     content: [
       {
         sessionId: 1,
@@ -106,139 +92,32 @@ export default function Home() {
     totalElements: "1",
   });
 
-  const handleClickChangeHistoryFilter = (filter: PTHistoryFilterTypes) => {
-    setPtHistoryFilter(filter);
-  };
-
-  const handleClickRouting = (path: string) => {
-    const currentPath = window.location.pathname;
-
-    if (currentPath === path) return;
-    router.push(`${currentPath}/${path}`);
-  };
-
-  const negative = {
-    label: "취소",
-    callback: () => {},
-  };
-
-  const positive = {
-    label: "로그아웃",
-    callback: () => {
-      router.push("/");
-    },
-  };
-
   return (
-    <main className="flex h-screen w-full flex-col overflow-hidden pb-[5.063rem]">
-      <section className="flex items-center justify-between">
-        <ProfileHeader>
-          <ProfileHeader.Section
-            onClick={() => {
-              handleClickRouting("/my-infomation");
-            }}
-          >
-            <ProfileHeader.Avatar name="홍길동" imageSrc={mockData.profilePictureUrl} />
-            <ProfileHeader.Name name="홍길동" />
-          </ProfileHeader.Section>
-        </ProfileHeader>
-        <Popup title="로그아웃을 하시겠습니까?" negative={negative} positive={positive}>
-          <PopupTrigger className="w-fit">
-            <Badge variant="sub2" className="px-4">
-              로그아웃
-            </Badge>
-          </PopupTrigger>
-        </Popup>
-      </section>
+    <main className="flex h-screen w-full flex-col overflow-hidden">
+      <ProfileHeader userInformation={mockData} />
 
-      <section className="mt-[1.563rem] flex h-auto flex-col">
-        <ProfileItem variant="dumbbell">
-          <ProfileItemContent>
-            <Badge>
-              {getFormattedPTCount(
-                mockData.sessionInfo.remainingCount,
-                mockData.sessionInfo.totalCount,
-              )}
-            </Badge>
-          </ProfileItemContent>
-        </ProfileItem>
-        <ProfileItem variant="trainer">
-          <ProfileItemContent
-            className="cursor-pointer"
-            onClick={() => {
-              if (mockData.trainerName !== "") {
-                handleClickRouting("/my-trainer-infomation");
-              } else {
-                handleClickRouting("/connect-trainer");
-              }
-            }}
-          >
-            <div
-              className={`flex items-center gap-0 ${mockData.trainerName ? "" : "text-text-primary"}`}
-            >
-              {mockData.trainerName ? mockData.trainerName : "연동하기"}
-              <ChevronRight />
-            </div>
-          </ProfileItemContent>
-        </ProfileItem>
-      </section>
+      <ProfileItem className="mt-[1.563rem]" variant="dumbbell">
+        <Badge>
+          {getFormattedPTCount(
+            mockData.sessionInfo.remainingCount,
+            mockData.sessionInfo.totalCount,
+          )}
+        </Badge>
+      </ProfileItem>
 
-      <section className="mt-[0.625rem] flex h-auto flex-col">
-        <Dropdown className="w-full">
-          <DropdownTrigger>
-            <div className="text-headline">PT희망 시간</div>
-          </DropdownTrigger>
-          <DropdownContent>
-            <DropdownItem>
-              <div className="">희망시간</div>
-            </DropdownItem>
-          </DropdownContent>
-        </Dropdown>
-      </section>
+      <ConnectedTrainerItem trainerName={mockData.trainerName} />
 
-      <section className="flex h-auto flex-col">
-        <Dropdown className="w-full">
-          <DropdownTrigger>
-            <div className="text-headline">PT고정 시간</div>
-          </DropdownTrigger>
-          <DropdownContent>
-            <DropdownItem>
-              <div className="">고정시간</div>
-            </DropdownItem>
-          </DropdownContent>
-        </Dropdown>
-      </section>
+      <ScheduleInformationItem
+        title="PT희망 시간"
+        className="mt-[0.625rem]"
+        schedules={mockData.workoutSchedules}
+      />
+      <ScheduleInformationItem title="PT고정 시간" schedules={mockData.workoutSchedules} />
 
-      <section>
-        <p className="text-headline mt-[1.625rem]">PT 내역</p>
-
-        <div className="mt-[0.625rem] flex items-center gap-2">
-          {Object.entries(FILTER_OPTIONS).map(([key, value]) => (
-            <Button
-              key={`PT-history-filter-${key}`}
-              size="sm"
-              variant={ptHistoryFilter === key ? "negative" : "secondary"}
-              onClick={() => handleClickChangeHistoryFilter(key as PTHistoryFilterTypes)}
-            >
-              {value}
-            </Button>
-          ))}
-        </div>
-      </section>
-
-      <section className="mt-[1.25rem] flex flex-col gap-[0.625rem] overflow-y-auto pb-2">
-        {mock_pt_history.content.map((item: PtInfo) => {
-          if (item.status !== ptHistoryFilter && ptHistoryFilter !== "ALL") return;
-
-          return (
-            <PTHistoryItem
-              key={`PT-history-item-${item.sessionId}`}
-              reservationDate={item.reservationDate}
-              status={item.status as "COMPLETED" | "NO_SHOW" | "NONE"}
-            />
-          );
-        })}
-      </section>
+      <PTHistoryProvider>
+        <PTHistoryFilter />
+        <PTHistoryContent ptHistory={ptHistory.content} />
+      </PTHistoryProvider>
     </main>
   );
 }
