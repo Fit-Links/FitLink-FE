96609b156c390f4ea07b272473d32f1b1d016385
MaSeungHyun <tpdlqj0514@gmail.com>
2025-04-04 02:58:23 +0900

    FLIZ-126 feat: 내 정보 조회 페이지 컴포넌트화로 구조 변경

---
+++ apps/user/app/my-page/my-information/_components/BottomSheet/EditProfileBottomSheet.tsx
@@ -0,0 +1,73 @@
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@ui/components/Sheet";
+import React, { useRef } from "react";
+
+import { MyInformationDetailApiResponse } from "@user/services/types/myInformation.dto";
+
+import SheetItem from "@user/components/SheetItem";
+
+interface EditProfileBottomSheetProps {
+  children: React.ReactNode;
+  onChangeMyInformation: (key: keyof MyInformationDetailApiResponse["data"], value: string) => void;
+}
+
+export default function EditProfileBottomSheet({
+  children,
+  onChangeMyInformation,
+}: EditProfileBottomSheetProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClickOpenAlbum = () => {
+    inputRef.current?.click();
+  };
+
+  const handleClickDeleteProfileImage = () => {
+    onChangeMyInformation("profilePictureUrl", "");
+  };
+
+  const handleChangeProfileImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (!file) return;
+
+    const uri = URL.createObjectURL(file as Blob);
+    onChangeMyInformation("profilePictureUrl", uri);
+  };
+
+  return (
+    <>
+      <input
+        ref={inputRef}
+        className="hidden"
+        type="file"
+        accept="image/*"
+        onChange={handleChangeProfileImage}
+      />
+      <Sheet>
+        <SheetTrigger asChild>{children}</SheetTrigger>
+        <SheetContent side={"bottom"}>
+          <SheetTitle></SheetTitle>
+          <SheetDescription className="flex flex-col gap-[0.625rem]">
+            <SheetClose asChild>
+              <SheetItem icon="Image" label="앨범에서 선택" onClick={handleClickOpenAlbum} />
+            </SheetClose>
+            <SheetClose asChild>
+              <SheetItem
+                icon="Trash2"
+                label="프로필 사진 삭제"
+                variant="danger"
+                onClick={handleClickDeleteProfileImage}
+              />
+            </SheetClose>
+          </SheetDescription>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+}
