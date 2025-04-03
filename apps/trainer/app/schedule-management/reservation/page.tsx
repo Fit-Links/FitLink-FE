445ff667e0b41dc914b0c2bf0d7263c9316b80b2
choi-ik <ci980704@naver.com>
2025-04-04 00:07:00 +0900

    FLIZ-170 feat: reservation(PT 예약) 루트 페이지 구현

---
+++ apps/trainer/app/schedule-management/reservation/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./_components/Header";
+import MemberListContainer from "./_components/MemberListContainer";
+
+function Reservation() {
+  return (
+    <main className="flex h-full flex-col">
+      <Header />
+      <MemberListContainer />
+    </main>
+  );
+}
+
+export default Reservation;
