1b78efd39e2d7a58ec3a4553c7805719d3282254
choi-ik <ci980704@naver.com>
2025-04-04 11:23:30 +0900

    FLIZ-170 feat: TimeOptionList 컴포넌트에서 고정 예약 페이지로 라우팅 기능 추가

--- apps/trainer/app/schedule-management/_components/TimeOptionList.tsx
+++ apps/trainer/app/schedule-management/_components/TimeOptionList.tsx
@@ -11,24 +11,31 @@ type TimeOptionListProps = {
 };
 
 export default function TimeOptionList({ selectedDate }: TimeOptionListProps) {
-  const { ROOT, RESERVATION } = ROUTES;
+  const { ROOT } = ROUTES;
 
   const router = useRouter();
 
-  const handleClickTimeOption = () => {
-    router.push(`${ROOT}${RESERVATION}?selectedDate=${selectedDate}`);
+  const handleClickTimeOption = (route: Exclude<keyof typeof ROUTES, "ROOT">) => {
+    switch (route) {
+      case "RESERVATION":
+        router.push(`${ROOT}${ROUTES[route]}?selectedDate=${selectedDate}`);
+        break;
+      case "FIXED_RESERVATION":
+        router.push(`${ROOT}${ROUTES[route]}`);
+        break;
+    }
   };
 
   // TODO: 추후 각 TimeOption 클릭 시 이동할 페이지의 경로 Name이 정해지면 클릭 이벤트 추가
   return (
     <div className="mb-[1.625rem] ml-[1.063rem] mt-[1.25rem] flex items-center gap-1.5 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-      <TimeOption onClick={handleClickTimeOption}>
+      <TimeOption onClick={() => handleClickTimeOption("RESERVATION")}>
         <TimeOption.Icon iconName={"Dumbbell"} />
         <TimeOption.Content>
           <div>PT 예약</div>
         </TimeOption.Content>
       </TimeOption>
-      <TimeOption>
+      <TimeOption onClick={() => handleClickTimeOption("FIXED_RESERVATION")}>
         <TimeOption.Icon iconName={"CalendarClock"} />
         <TimeOption.Content>
           <div>PT</div>
