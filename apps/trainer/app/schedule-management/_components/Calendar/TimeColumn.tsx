8e47e2e9fa1d642429f60ac4a797dcc4c742dd42
choi-ik <ci980704@naver.com>
2025-04-08 16:16:06 +0900

    FLIZ-170 style: 캘린더의 시간 column을 나타내는 컴포넌트의 marin 변경

--- apps/trainer/app/schedule-management/_components/Calendar/TimeColumn.tsx
+++ apps/trainer/app/schedule-management/_components/Calendar/TimeColumn.tsx
@@ -12,7 +12,7 @@ export default function TimeColumn() {
   });
 
   return (
-    <div className="text-body-4 bg-background-primary text-text-primary left-0 z-10 mr-1 flex h-max flex-col gap-[0.0625rem]">
+    <div className="text-body-4 bg-background-primary text-text-primary left-0 z-10 mr-2 flex h-max flex-col gap-[0.0625rem]">
       {TimeCoulmns.map((time) => (
         <div className=" flex h-[3.9375rem] w-[2.625rem] justify-end font-mono" key={time}>
           {time}
