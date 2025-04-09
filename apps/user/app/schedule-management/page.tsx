4402e7675d9853e55ce1115e1b1576165f40c310
choi-ik <ci980704@naver.com>
2025-04-09 11:46:38 +0900

    FLIZ-121 chore: 변경된 컴포넌트 네이밍에 맞게 수정

--- apps/user/app/schedule-management/page.tsx
+++ apps/user/app/schedule-management/page.tsx
@@ -1,5 +1,5 @@
-import AddReservation from "./_components/AddReservation";
 import Calendar from "./_components/Calendar";
+import ReservationAdder from "./_components/ReservationAdder";
 
 function ScheduleManagement() {
   /** TODO: 트레이너 연동 인증 로직으로 구현 */
@@ -8,7 +8,7 @@ function ScheduleManagement() {
     <main className="relative flex h-full w-full justify-center overflow-y-auto">
       {/* <Link href={"http://3.34.169.105/oauth2/authorization/kakao"}>카카오 로그인</Link> */}
       <Calendar />
-      <AddReservation trainerConnectStatus="CONNECTED" ptCount={1} />
+      <ReservationAdder trainerConnectStatus="CONNECTED" ptCount={1} />
     </main>
   );
 }
