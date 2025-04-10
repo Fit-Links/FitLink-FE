0fe3d698b18ddb08ec0bb095e6c183237576a1e8
choi-ik <ci980704@naver.com>
2025-04-10 10:19:58 +0900

    FLIZ-196 fix: cicd 액션 실행시 개인 리포지토리로 숨김파일은 제거되고 clone 되는 에러 수정

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 cd ../
-# Ensure a clean slate
+rm -rf output
 mkdir output
 
-# Copy necessary content to the output directory
+shopt -s dotglob
+
 cp -R ./FitLink-FE/* ./output
 cp -R ./output ./FitLink-FE/
