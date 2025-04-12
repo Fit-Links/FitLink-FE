b371d398640d04466fbfe6067a4b914ce1aca3b2
MaSeungHyun <tpdlqj0514@gmail.com>
2025-04-12 19:01:51 +0900

    FLIZ-187 feat: 레이아웃 역할을 위한 section에서 div태그로 변경

--- packages/ui/src/components/PhoneVerification/PhoneVerificationNotice.tsx
+++ packages/ui/src/components/PhoneVerification/PhoneVerificationNotice.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 export default function PhoneVerificationNotice() {
   return (
-    <main className="text-body-2 text-text-sub2 mt-[2.5rem] w-full text-center">
+    <div className="text-body-2 text-text-sub2 mt-[2.5rem] w-full text-center">
       <p>이용 중인 통신 요금제에 따라 문자 메시지 발송 비용이</p>
       <p>청구될 수 있어요.</p>
-    </main>
+    </div>
   );
 }
