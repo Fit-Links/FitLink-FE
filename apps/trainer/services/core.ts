8762192af0416df90bc3acd69b661bc923dfdabe
yjc2021 <yongjae.choi20@gmail.com>
2025-04-15 15:04:50 +0900

    FLIZ-184 feat: trainer tokenProvider 로직 구현

--- apps/trainer/services/core.ts
+++ apps/trainer/services/core.ts
@@ -1,4 +1,7 @@
 import http, { initCoreApi } from "@5unwan/core/api/core";
+import { getLocalStorage } from "@5unwan/core/utils/localStorage";
+
+import { ACCESS_TOKEN_KEY } from "@trainer/constants/token";
 
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -8,6 +11,10 @@ if (!baseUrl) {
 
 /** TODO: 토큰을 어떻게 가져올 것인지 로그인 기능 담당자가 추후 구현 */
 const tokenProvider = () => {
+  const accessToken = getLocalStorage(ACCESS_TOKEN_KEY);
+
+  if (typeof accessToken === "string") return accessToken;
+
   return null;
 };
 
