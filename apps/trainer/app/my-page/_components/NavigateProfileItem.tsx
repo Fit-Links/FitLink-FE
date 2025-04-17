f53e699d35af7218c44b1efd5d6d4e8acffdd1fb
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-04-17 12:11:53 +0900

    FLIZ-191 feat: Trainer 마이페이지 메인 UI 구현

---
+++ apps/trainer/app/my-page/_components/NavigateProfileItem.tsx
@@ -0,0 +1,31 @@
+import { ProfileItem, ProfileItemVariants } from "@ui/components/ProfileItem";
+import { ChevronRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useCallback } from "react";
+
+import { MYPAGE_ROUTES } from "@trainer/constants/mypageRoute";
+
+const NAVIGATE_ROUTE = {
+  code: MYPAGE_ROUTES.CODE,
+  phone: MYPAGE_ROUTES.PHONE,
+  myInformation: MYPAGE_ROUTES.MY_INFORMATION,
+  editMyWorkoutSchedules: MYPAGE_ROUTES.EDIT_MY_WORKOUT_SCHEDULES,
+} as const;
+
+type NavigateProfileItemProps = {
+  className?: string;
+  variant: keyof typeof ProfileItemVariants;
+};
+export default function NavigateProfileItem({ className, variant }: NavigateProfileItemProps) {
+  const router = useRouter();
+
+  const handleClickRoute = useCallback(() => {
+    router.push(NAVIGATE_ROUTE[variant as keyof typeof NAVIGATE_ROUTE]);
+  }, [variant]);
+
+  return (
+    <ProfileItem className={className} variant={variant}>
+      <ChevronRight onClick={handleClickRoute} />
+    </ProfileItem>
+  );
+}
