9e9439fc2bdc1101b11e3d72b09e84a21b0f74c1
choi-ik <ci980704@naver.com>
2025-04-17 13:02:27 +0900

    FLIZ-206 feat: 레이아웃 및 Provider 구헌 -> #116 PR에서 구현한 레이아웃과 Provider로 교체될 예정

--- apps/trainer/app/layout.tsx
+++ apps/trainer/app/layout.tsx
@@ -1,3 +1,4 @@
+import Providers from "@trainer/components/Providers";
 import "./global.css";
 
 export default function RootLayout({
@@ -6,8 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body className="bg-background-primary text-text-primary md:border-background-sub2 md:max-w-mobile relative box-content h-screen w-full md:mx-auto md:overflow-hidden md:border md:shadow-lg">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
