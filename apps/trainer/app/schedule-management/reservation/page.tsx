2e77f1af42a6c62e5c77fcf9bfd7767f5ffe4d45
choi-ik <ci980704@naver.com>
2025-04-20 15:53:32 +0900

    FLIZ-210 chore: 누락된 Todo 주석 추가

--- apps/trainer/app/schedule-management/reservation/page.tsx
+++ apps/trainer/app/schedule-management/reservation/page.tsx
@@ -7,6 +7,7 @@ import ReservationAdderButton from "./_components/ReservationAdderButton";
 function Reservation() {
   return (
     <main className="flex h-full flex-col">
+      {/** TODO: prerender 오류 방지를 위한 임시 fallback으로 추후 수정 */}
       <Suspense fallback={<div>preRender 오류 방지를 위한 임시 fallback</div>}>
         <Header />
       </Suspense>
