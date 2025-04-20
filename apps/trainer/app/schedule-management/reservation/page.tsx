4afa362565d58e925516ecf9a321747611609572
choi-ik <ci980704@naver.com>
2025-04-20 12:18:45 +0900

    FLIZ-210 chore: 트레이너 도메인 preRender 오류 방지를 위한 Suspense 설정과 변수 참조 에러 수정

--- apps/trainer/app/schedule-management/reservation/page.tsx
+++ apps/trainer/app/schedule-management/reservation/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import MemberListContainer from "../_components/MemberListContainer";
 import Header from "./_components/Header";
 import ReservationAdderButton from "./_components/ReservationAdderButton";
@@ -5,7 +7,9 @@ import ReservationAdderButton from "./_components/ReservationAdderButton";
 function Reservation() {
   return (
     <main className="flex h-full flex-col">
-      <Header />
+      <Suspense fallback={<div>preRender 오류 방지를 위한 임시 fallback</div>}>
+        <Header />
+      </Suspense>
       <MemberListContainer renderFooterReservationButton={ReservationAdderButton} />
     </main>
   );
