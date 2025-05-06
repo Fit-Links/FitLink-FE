dd844fc250ada5bc69f744fa5d98a5bb231a4fe8
MaSeungHyun <tpdlqj0514@gmail.com>
2025-05-06 18:40:05 +0900

    FLIZ-224 feat: 내 트레이너 정보 조회 컨테이너화 및 api 연결

--- apps/user/app/my-page/my-trainer-information/page.tsx
+++ apps/user/app/my-page/my-trainer-information/page.tsx
@@ -1,40 +1,21 @@
-"use client";
+import { HydrationBoundary } from "@tanstack/react-query";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@ui/components/Avatar";
-import { ProfileItem } from "@ui/components/ProfileItem";
-import { useState } from "react";
+import { myInformationQueries } from "@user/queries/myInformation";
 
-import { MyInformationDetailApiResponse } from "@user/services/types/myInformation.dto";
+import { getQueryClient } from "@user/components/Providers/queryClient";
 
-import Header from "../_components/Header";
-import TrainerUnlinkItem from "./_components/TrainerUnlinkItem";
+import MyTrainerInformationContainer from "./_components/MyTrainerInformationContainer";
 
-export default function MyTrainerInformation() {
-  const [mockData] = useState<MyInformationDetailApiResponse["data"]>({
-    memberId: 2,
-    name: "김민수",
-    birthDate: "1990-05-12",
-    phoneNumber: "010-2938-2312",
-    profilePictureUrl: "https://github.com/shadcn.png",
-  });
+export default async function MyTrainerInformation() {
+  const queryClient = getQueryClient();
+
+  const data = await queryClient.prefetchQuery(myInformationQueries.detail());
 
   return (
     <main className="flex h-screen w-full flex-col items-center overflow-hidden pb-[5.063rem]">
-      <Header title="트레이너" />
-
-      <Avatar className="mt-[1.563rem]  h-[6.313rem] w-[6.313rem]">
-        <AvatarFallback />
-        <AvatarImage src={mockData.profilePictureUrl} />
-      </Avatar>
-
-      <ProfileItem className="w-full" variant={"name"}>
-        {mockData.name}
-      </ProfileItem>
-      <ProfileItem className="w-full" variant={"phone"}>
-        {mockData.phoneNumber}
-      </ProfileItem>
-
-      <TrainerUnlinkItem />
+      <HydrationBoundary state={data}>
+        <MyTrainerInformationContainer />
+      </HydrationBoundary>
     </main>
   );
 }
