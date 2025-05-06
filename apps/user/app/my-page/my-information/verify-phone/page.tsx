893d256a1e6d7884b347d1c89a0d9808f5f7f264
MaSeungHyun <tpdlqj0514@gmail.com>
2025-05-06 18:27:17 +0900

    FLIZ-224 feat: 휴대폰 번호 변경 인증 UI 작업 해당 api 기능으로 인증 변경에 대한 문제가 발생하여 api는 적용되지 않고 테스트 코드로 존재

--- apps/user/app/my-page/my-information/verify-phone/page.tsx
+++ apps/user/app/my-page/my-information/verify-phone/page.tsx
@@ -1,16 +1,46 @@
 "use client";
 
-import { Button } from "@ui/components/Button";
 import React from "react";
 
+import VerificationPhoneContainer from "./_components/VerificationPhoneContainer";
 import Header from "../../_components/Header";
 
+// const REFETCH_INTERVAL = 5000;
+
+const MOCK_TOKEN = "1234567890";
+
 export default function VerifyPhone() {
+  // const navigation = useRouter();
+
+  // const [hasClicked, setHasClicked] = useState(false);
+
+  const handleClick = () => {
+    //   setHasClicked(true);
+  };
+
+  // const { data: tokenData, isPending: isTokenPending } = useQuery(authQueries.snsToken());
+
+  // const { data: statusData } = useQuery({
+  //   ...authQueries.status(),
+  //   enabled: hasClicked,
+  //   refetchInterval: (query) => {
+  //     return REFETCH_INTERVAL;
+  //   },
+  // });
+
+  // if (statusData && statusData.data) {
+  //   const { status } = statusData.data;
+
+  //   TODO: 휴대폰 변경 요청 상태 또한 필요로 되어 보임
+  //   // REGISTER가 아닌 휴대폰 변경 요청 상태 또한 필요로 되어 보임
+
+  //   navigation.back();
+  // }
+
   return (
     <main className="flex h-screen w-full flex-col items-center justify-between">
       <Header title="휴대폰 인증" />
-
-      <Button className="relative bottom-[2.125rem] h-[3.375rem] w-full">변경</Button>
+      <VerificationPhoneContainer onClick={handleClick} verificationToken={MOCK_TOKEN} />
     </main>
   );
 }
