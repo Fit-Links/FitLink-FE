3784ccf4c1a3d81d88ae596f3724c198828efab9
MaSeungHyun <tpdlqj0514@gmail.com>
2025-05-09 01:50:46 +0900

    FLIZ-224 feat: PT 희망시간 컨테이너 및 Elipsis에 대한 바텀 시트 적용

--- apps/user/app/my-page/_components/PTInformation/ScheduleContainer.tsx
+++ apps/user/app/my-page/_components/PTInformation/ScheduleContainer.tsx
@@ -1,12 +1,12 @@
-import Icon from "@ui/components/Icon";
 import React from "react";
 
-export default function ScheduleContainer({ children }: { children: React.ReactNode }) {
+type ScheduleContainerProps = {
+  children: React.ReactNode;
+};
+
+export default function ScheduleContainer({ children }: ScheduleContainerProps) {
   return (
     <section className="bg-background-sub1 relative min-h-10 w-full rounded-lg p-[1.25rem]">
-      <div className="absolute right-0 top-0 flex h-[2.5rem] w-[2.5rem] cursor-pointer items-center">
-        <Icon name="Ellipsis" className="text-background-sub4 h-[1.5625rem] w-[1.5625rem]" />
-      </div>
       {children}
     </section>
   );
