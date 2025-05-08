3403fc9add51d748a11ee6e9b6c74080efc0e32b
MaSeungHyun <tpdlqj0514@gmail.com>
2025-05-09 01:47:17 +0900

    FLIZ-224 feat: 연결된 트레이너 표시 기준 Name에서 Id로 변경 및 api 응답 타입 추가

--- apps/user/app/my-page/_components/PTInformation/ConnectedTrainerItem.tsx
+++ apps/user/app/my-page/_components/PTInformation/ConnectedTrainerItem.tsx
@@ -1,16 +1,19 @@
+"use client";
+
+import Icon from "@ui/components/Icon";
 import { ProfileItem, ProfileItemContent } from "@ui/components/ProfileItem";
 import { cn } from "@ui/lib/utils";
-import { ChevronRight } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 import RouteInstance from "@user/constants/routes";
 
 interface TrainerInformationProps {
-  trainerName: string;
+  trainerId: number | null;
+  trainerName: string | null;
 }
 
-export default function ConnectedTrainerItem({ trainerName }: TrainerInformationProps) {
+export default function ConnectedTrainerItem({ trainerId, trainerName }: TrainerInformationProps) {
   const router = useRouter();
 
   const handleClickRouting = (path: string) => {
@@ -19,22 +22,22 @@ export default function ConnectedTrainerItem({ trainerName }: TrainerInformation
 
   return (
     <ProfileItem variant="trainer">
-      <ProfileItemContent className="cursor-pointer">
+      <ProfileItemContent>
         <div
           className={cn(
             "flex items-center gap-0",
-            trainerName ? "text-text-sub2" : "text-text-primary",
+            trainerId ? "text-text-sub2" : "text-text-primary",
           )}
           onClick={() => {
             handleClickRouting(
-              trainerName
+              trainerId
                 ? RouteInstance["my-trainer-information"]()
                 : RouteInstance["connect-trainer"](),
             );
           }}
         >
-          {trainerName ? trainerName : "연동하기"}
-          <ChevronRight />
+          {trainerId ? trainerName : "연동하기"}
+          <Icon name="ChevronRight" className="cursor-pointer" size="lg" />
         </div>
       </ProfileItemContent>
     </ProfileItem>
