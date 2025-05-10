1eee82bc281db731bc648c14b68c51f33d942421
MaSeungHyun <tpdlqj0514@gmail.com>
2025-05-10 16:36:32 +0900

    FLIZ-225 feat: 휴무일 삭제  api 적용

--- apps/trainer/app/my-page/_components/MyDayOffContainer.tsx
+++ apps/trainer/app/my-page/_components/MyDayOffContainer.tsx
@@ -1,14 +1,24 @@
 "use client";
 
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { DayOfWeek } from "@5unwan/core/api/types/common";
+import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
 import Icon from "@ui/components/Icon";
 import React from "react";
 
 import { myInformationQueries } from "@trainer/queries/myInformation";
 
+import { deleteTimeOff } from "@trainer/services/myInformation";
+import { DeleteTimeOffRequestBody } from "@trainer/services/types/myInformation.dto";
+
 import { DAYS } from "@trainer/constants/Day";
 
 const NoDayOff = 0;
+
+type DayOffInformation = {
+  dayOffId: number;
+  dayOffDate: string;
+  dayOfWeek: string;
+};
 
 export default function MyDayOffContainer() {
   const { data: response } = useSuspenseQuery(myInformationQueries.dayOff());
@@ -26,24 +36,54 @@ export default function MyDayOffContainer() {
     };
   });
 
-  //   const { mutate } = useMutation({
-  //     mutationFn: deleteTimeOff,
-  //   });
+  const { mutate, isSuccess, isError } = useMutation({
+    mutationFn: (params: {
+      requestPath: { dayOffId: number };
+      requestBody: DeleteTimeOffRequestBody;
+    }) => deleteTimeOff(params.requestPath, params.requestBody),
+  });
 
-  //   const handleDeleteDayOff = (dayOff: string) => {
-  //     mutate({ dayOffId });
-  //   };
+  const handleDeleteDayOff = (holiday: DayOffInformation) => {
+    mutate(
+      {
+        requestPath: { dayOffId: holiday.dayOffId },
+        requestBody: {
+          dayOfWeek: holiday.dayOfWeek as DayOfWeek,
+          dayOfTime: holiday.dayOffDate,
+        },
+      },
+      {
+        onSuccess: () => {
+          // TODO:
+
+          if (isSuccess) {
+            console.log("성공적으로 삭제되었습니다.");
+          }
+        },
+        onError: (error) => {
+          if (isError) {
+            console.error("삭제 중 오류가 발생했습니다:", error);
+          }
+        },
+      },
+    );
+  };
 
   return (
     <section className="mt-[1.563rem] w-full">
       <p className="text-headline mb-[0.625rem]">휴무일</p>
       <section className="flex flex-col gap-1">
-        {holidayList.map(({ dayOfWeek, dayOffDate }) => (
+        {holidayList.map((holiday) => (
           <div className="bg-background-sub1 text-text-primary flex h-[3.375rem] w-full items-center justify-between rounded-lg px-[0.9375rem]">
             <span>
-              {dayOffDate} {DAYS[dayOfWeek as keyof typeof DAYS]}
+              {holiday.dayOffDate} {DAYS[holiday.dayOfWeek as keyof typeof DAYS]}
             </span>
-            <Icon name="X" size="lg" className="cursor-pointer" />
+            <Icon
+              name="X"
+              size="lg"
+              className="cursor-pointer"
+              onClick={() => handleDeleteDayOff(holiday)}
+            />
           </div>
         ))}
       </section>
