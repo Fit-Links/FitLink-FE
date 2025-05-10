8b1c2587c0ce8e91102905ca5c352b4100e9d3d0
MaSeungHyun <tpdlqj0514@gmail.com>
2025-05-10 16:22:01 +0900

    FLIZ-225 feat: trainerCode  Input을 가운데로 정렬 후 아이콘 배치 및 6자리에 따른 글자 크기 조정

--- apps/trainer/app/my-page/trainer-code/_components/TrainerCodeContainer/index.tsx
+++ apps/trainer/app/my-page/trainer-code/_components/TrainerCodeContainer/index.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { useSuspenseQuery } from "@tanstack/react-query";
 
+import Header from "@trainer/app/my-page/_components/Header";
 import { myInformationQueries } from "@trainer/queries/myInformation";
 
 import InputWithCopy from "./InputWithCopy";
@@ -9,6 +12,7 @@ export default function TrainerCodeContainer() {
 
   return (
     <section className="flex w-full flex-col items-center">
+      <Header title="트레이너 코드" />
       <p className="text-text-sub4 text-body-1 mt-[0.625rem]">
         회원에게 코드를 복사해 공유해 주세요
       </p>
