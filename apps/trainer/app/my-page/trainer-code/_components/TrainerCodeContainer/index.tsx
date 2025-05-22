20bea732f990b2b02dc16e131541c1036dc96b55
MaSeungHyun <tpdlqj0514@gmail.com>
2025-05-23 04:32:43 +0900

    FLIZ-225 feat: 트레이너 코드 조회 API 연결

--- apps/trainer/app/my-page/trainer-code/_components/TrainerCodeContainer/index.tsx
+++ apps/trainer/app/my-page/trainer-code/_components/TrainerCodeContainer/index.tsx
@@ -10,13 +10,15 @@ import InputWithCopy from "./InputWithCopy";
 export default function TrainerCodeContainer() {
   const { data: response } = useSuspenseQuery(myInformationQueries.trainerCode());
 
+  const trainerCode = response?.data.trainerCode;
+
   return (
     <section className="flex w-full flex-col items-center">
       <Header title="트레이너 코드" />
       <p className="text-text-sub4 text-body-1 mt-[0.625rem]">
         회원에게 코드를 복사해 공유해 주세요
       </p>
-      <InputWithCopy value={response.trainerCode} />
+      <InputWithCopy value={trainerCode} />
     </section>
   );
 }
--- apps/trainer/app/my-page/trainer-code/page.tsx
+++ apps/trainer/app/my-page/trainer-code/page.tsx
@@ -1,20 +1,11 @@
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import React from "react";
-
-import { myInformationQueries } from "@trainer/queries/myInformation";
 
 import TrainerCodeContainer from "./_components/TrainerCodeContainer";
 
 export default async function TrainnerCode() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery(myInformationQueries.trainerCode());
-
   return (
     <main className="bg-background-primary text-text-primary flex h-screen w-full flex-col items-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <TrainerCodeContainer />
-      </HydrationBoundary>
+      <TrainerCodeContainer />
     </main>
   );
 }
