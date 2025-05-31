efd6163448cef1a461398deec7b2871109c3144b
MaSeungHyun <tpdlqj0514@gmail.com>
2025-05-31 18:40:01 +0900

    FLIZ-284 chore: 불필요한 console.log() 제거

--- apps/user/app/my-page/_components/MyPageContainer.tsx
+++ apps/user/app/my-page/_components/MyPageContainer.tsx
@@ -45,8 +45,6 @@ export default function MyPageContainer() {
 
   const formattedFixedSchedule = getISOToKoreanTime(uniqueFixedSchedule);
 
-  console.log(myInformation);
-
   return (
     <div>
       <ProfileHeader
