4c3b0cc1a2796965482b50b9e041150dbb852590
MaSeungHyun <tpdlqj0514@gmail.com>
2025-06-29 02:27:27 +0900

    FLIZ-396 chore: 서버와 클라이언트 컴포넌트에서 모든 date 테스트 구현

--- apps/trainer/app/login/_components/LoginButton/ClientDate.tsx
+++ apps/trainer/app/login/_components/LoginButton/ClientDate.tsx
@@ -6,11 +6,17 @@ import { getKoreanDate } from "@trainer/utils/date";
 
 function ClientDate() {
   const time = getKoreanDate("2025-05-10T15:00:00");
+  const time2 = getKoreanDate("2025-05-10");
+  const time3 = getKoreanDate();
 
   return (
     <>
       <p suppressHydrationWarning>{time.toString()}</p>
       <p suppressHydrationWarning>{time.toLocaleString()}</p>
+      <p suppressHydrationWarning>{time2.toString()}</p>
+      <p suppressHydrationWarning>{time2.toLocaleString()}</p>
+      <p suppressHydrationWarning>{time3.toString()}</p>
+      <p suppressHydrationWarning>{time3.toLocaleString()}</p>
     </>
   );
 }
--- apps/trainer/app/login/page.tsx
+++ apps/trainer/app/login/page.tsx
@@ -10,11 +10,17 @@ import NaverContent from "./_components/LoginButton/NaverContent";
 
 function LoginPage() {
   const date = getKoreanDate("2025-05-10T15:00:00");
+  const date2 = getKoreanDate("2025-05-10");
+  const date3 = getKoreanDate();
 
   return (
     <main className="flex h-full flex-col justify-between pb-[3.375rem] pt-[6.25rem]">
       <p suppressHydrationWarning>{date.toString()}</p>
       <p suppressHydrationWarning>{date.toLocaleString()}</p>
+      <p suppressHydrationWarning>{date2.toString()}</p>
+      <p suppressHydrationWarning>{date2.toLocaleString()}</p>
+      <p suppressHydrationWarning>{date3.toString()}</p>
+      <p suppressHydrationWarning>{date3.toLocaleString()}</p>
       <section>
         <h1 className="text-[1.6875rem] font-medium">
           가장 편한 방법으로 <br />
