181aa22e2b074a7078cb150e3cb58fa934356a33
MaSeungHyun <tpdlqj0514@gmail.com>
2025-06-29 01:41:36 +0900

    FLIZ-396 chore: 리다이렉션으로 인한 login 페이지에서 데이트 테스트 프리뷰 코드 적용

---
+++ apps/trainer/app/login/_components/LoginButton/ClientDate.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+
+import { getKoreanDate } from "@trainer/utils/date";
+
+function ClientDate() {
+  const time = getKoreanDate();
+
+  return (
+    <>
+      <p suppressHydrationWarning>{time.toString()}</p>
+      <p suppressHydrationWarning>{time.toLocaleString()}</p>
+    </>
+  );
+}
+
+export default ClientDate;
