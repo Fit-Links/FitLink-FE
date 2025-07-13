fb58da9983f6d949f1e1deaf9db1d7b08fe27d23
MaSeungHyun <tpdlqj0514@gmail.com>
2025-07-13 10:44:46 +0900

    FLIZ-414 chore: BottomNavigation  자식값으로 이동

--- apps/trainer/components/Providers/FooterProvider.tsx
+++ apps/trainer/components/Providers/FooterProvider.tsx
@@ -27,16 +27,16 @@ function FooterProvider({ children }: { children: React.ReactNode }) {
     <>
       <div
         className={cn(
-          "bg-background-primary text-text-primary md:border-background-sub2 md:max-w-mobile relative mx-0 box-content flex min-h-[calc(100%-5.063rem)] flex-col px-4 md:mx-auto md:border md:shadow-lg",
+          "bg-background-primary text-text-primary md:border-background-sub2 md:max-w-mobile relative mx-0 box-content flex min-h-[calc(100%-5.063rem)] flex-col  md:mx-auto md:border md:shadow-lg",
           {
             "min-h-[calc(100%-5.063rem)] pb-[5.063rem]": hasFooter,
             "min-h-[calc(100%-2.125rem)] pb-[2.125rem]": !hasFooter,
           },
         )}
       >
-        {children}
+        <div className="h-full w-full px-4">{children}</div>
+        {hasFooter && <BottomNavigation />}
       </div>
-      {hasFooter && <BottomNavigation />}
     </>
   );
 }
