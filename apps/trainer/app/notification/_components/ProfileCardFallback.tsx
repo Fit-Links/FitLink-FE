9dfc2e787741535aedea1edce0d6be000fe38acf
yjc2021 <yongjae.choi20@gmail.com>
2025-08-01 11:48:30 +0900

    FLIZ-454 style: ProfileCardFallback y축padding 설정

--- apps/trainer/app/notification/_components/ProfileCardFallback.tsx
+++ apps/trainer/app/notification/_components/ProfileCardFallback.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@ui/components/Skeleton";
 
 function ProfileCardFallback() {
   return (
-    <div className="bg-background-sub1 flex h-[5.625rem] items-center gap-[10px] rounded-[0.625rem] px-[20px]">
+    <div className="bg-background-sub1 flex h-[5.625rem] items-center gap-[10px] rounded-[0.625rem] px-[20px] py-[1.25rem]">
       <Skeleton className="h-[3.125rem] w-[3.125rem] rounded-full " />
       <div className="flex h-[40px] flex-col justify-between">
         <Skeleton className="h-[17px] w-[50px]" />
